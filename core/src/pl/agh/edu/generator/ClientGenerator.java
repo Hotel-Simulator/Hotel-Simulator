efd5acec454f1040ac3d7e29b7372eb0366cca00
pwgacek <pawelgacek@student.agh.edu.pl>
2023-05-04 15:41:52 +0200

    created simple clientGenerator

--- core/src/pl/agh/edu/generator/ClientGenerator.java
+++ core/src/pl/agh/edu/generator/ClientGenerator.java
@@ -1,6 +1,19 @@
 package pl.agh.edu.generator;
 
+import org.json.simple.parser.ParseException;
+import pl.agh.edu.enums.HotelVisitPurpose;
+import pl.agh.edu.enums.RoomRank;
+import pl.agh.edu.enums.Sex;
+import pl.agh.edu.model.Client;
+import pl.agh.edu.model.ClientGroup;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.IntStream;
+
 public class ClientGenerator {
+
     /*
     WIZJA
     mamy plik, który mowi jaki procent klientow jest danego typu np: turysci: 80%, biznesmeni 15%, kuracjusze 5%
@@ -19,4 +32,82 @@ public class ClientGenerator {
     pomysł: na podstawie standardu miejsca (pokoj i recepcja) klient wystawia ilosc gwiazdek, ktora wpływa na popularność (razem z reklamą)
     pomysł: w wakacje i ferie bedzie bonus do turystow
      */
+
+    private static ClientGenerator clientGeneratorInstance;
+
+    private final Random random;
+    private HashMap<String, Long> attractivenessConstants;
+    private List<HotelVisitPurpose> hotelVisitPurposeProbabilityList;
+    private EnumMap<HotelVisitPurpose, List<Integer>> roomSizeProbabilityLists;
+    private EnumMap<HotelVisitPurpose, List<RoomRank>> desiredRoomRankProbabilityLists;
+    private EnumMap<HotelVisitPurpose, List<Integer>> numberOfNightsProbabilityLists;
+    private EnumMap<RoomRank, Map<Integer, Integer>> averagePricesPerNight;
+
+
+    private ClientGenerator() throws IOException, ParseException {
+        random = new Random();
+
+        attractivenessConstants = JSONExtractor.getAttractivenessConstantsFromJSON();
+        averagePricesPerNight = JSONExtractor.getAveragePricesPerNightFromJSON();
+
+        generateProbabilityLists();
+    }
+
+    private void generateProbabilityLists() throws IOException, ParseException {
+        hotelVisitPurposeProbabilityList = getProbabilityList(JSONExtractor.getHotelVisitPurposeProbabilitiesFromJSON());
+
+        roomSizeProbabilityLists = new EnumMap<>(HotelVisitPurpose.class);
+        for(Map.Entry<HotelVisitPurpose, Map<Integer, Integer>> entry : JSONExtractor.getRoomSizeProbabilitiesFromJSON().entrySet()){
+            roomSizeProbabilityLists.put(entry.getKey(),getProbabilityList(entry.getValue()));
+        }
+        desiredRoomRankProbabilityLists = new EnumMap<>(HotelVisitPurpose.class);
+        for(Map.Entry<HotelVisitPurpose, EnumMap<RoomRank, Integer>> entry : JSONExtractor.getDesiredRoomRankProbabilitiesFromJSON().entrySet()){
+            desiredRoomRankProbabilityLists.put(entry.getKey(),getProbabilityList(entry.getValue()));
+        }
+        numberOfNightsProbabilityLists = new EnumMap<>(HotelVisitPurpose.class);
+        for(Map.Entry<HotelVisitPurpose, Map<Integer, Integer>> entry : JSONExtractor.getNumberOfNightsProbabilitiesFromJSON().entrySet()){
+            numberOfNightsProbabilityLists.put(entry.getKey(),getProbabilityList(entry.getValue()));
+        }
+    }
+
+    public static ClientGenerator getClientGeneratorInstance() throws IOException, ParseException {
+        if(clientGeneratorInstance == null) clientGeneratorInstance = new ClientGenerator();
+        return clientGeneratorInstance;
+    }
+
+    private <T> List<T>  getProbabilityList(Map<T,Integer> map){
+        List<T> list = new ArrayList<>();
+        for(Map.Entry<T,Integer> entry : map.entrySet()){
+            for(int i=0;i<entry.getValue();i++) list.add(entry.getKey());
+        }
+        return list;
+    }
+
+    private <T> T getRandomValue(List<T> list){
+        return list.get(random.nextInt(0,list.size()));
+    }
+
+    public ClientGroup generateClientGroup(LocalDateTime time){
+        HotelVisitPurpose hotelVisitPurpose = getRandomValue(hotelVisitPurposeProbabilityList);
+
+        int roomSize = getRandomValue(roomSizeProbabilityLists.get(hotelVisitPurpose));
+        RoomRank desiredRoomRank  = getRandomValue(desiredRoomRankProbabilityLists.get(hotelVisitPurpose));
+        int numberOfNight = getRandomValue(numberOfNightsProbabilityLists.get(hotelVisitPurpose));
+        int desiredPricePerNight = averagePricesPerNight.get(desiredRoomRank).get(roomSize);
+        List<Client> members = new ArrayList<>();
+        for(int i=0;i<roomSize;i++){
+            members.add(new Client(random.nextInt(1,99), Sex.values()[random.nextInt(0,3)], hotelVisitPurpose));
+        }
+
+        return new ClientGroup(hotelVisitPurpose,members,time.plusDays(numberOfNight),desiredPricePerNight,desiredRoomRank);
+    }
+
+    public static void main(String[] args) throws IOException, ParseException {
+       ClientGenerator generator = getClientGeneratorInstance();
+       for(int i=0;i<10;i++){
+           System.out.println(generator.generateClientGroup(LocalDateTime.now()));
+       }
+    }
+
+
 }
