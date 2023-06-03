48fe7d0f9a1d812c0c653e3fabaa76097874cba7
pwgacek <pawelgacek@student.agh.edu.pl>
2023-06-03 19:57:44 +0200

    refactored JSONExtractor

--- core/src/pl/agh/edu/generator/client_generator/JSONExtractor.java
+++ core/src/pl/agh/edu/generator/client_generator/JSONExtractor.java
@@ -6,7 +6,6 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import pl.agh.edu.enums.HotelVisitPurpose;
 import pl.agh.edu.enums.RoomRank;
-import pl.agh.edu.model.Hotel;
 import pl.agh.edu.model.advertisement.json_data.ConstantAdvertisementData;
 import pl.agh.edu.model.advertisement.ConstantAdvertisementType;
 import pl.agh.edu.model.advertisement.SingleAdvertisementType;
@@ -16,127 +15,165 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class JSONExtractor {
     private static final JSONParser parser = new JSONParser();
     private static final String filePath = "assets/jsons/data.json";
 
     public static EnumMap<HotelVisitPurpose,Double> getHotelVisitPurposeProbabilitiesFromJSON() throws IOException, ParseException {
-        EnumMap<HotelVisitPurpose,Double> probabilities = new EnumMap<>(HotelVisitPurpose.class);
         JSONObject jsonObject = (JSONObject)((JSONObject) parser.parse(new FileReader(filePath))).get("hotel_visit_purpose_probabilities");
-        for(HotelVisitPurpose hotelVisitPurpose : HotelVisitPurpose.values()){
-            double probability = (double)jsonObject.get(hotelVisitPurpose.toString());
-            probabilities.put(hotelVisitPurpose,probability);
-        }
-        return probabilities;
+        return Stream.of(HotelVisitPurpose.values()).collect(Collectors.toMap(
+                e -> e,
+                e -> (double)jsonObject.get(e.toString()),
+                (a,b) -> b,
+                () -> new EnumMap<>(HotelVisitPurpose.class)
+        ));
     }
 
     public static EnumMap<HotelVisitPurpose,Map<RoomRank,Integer>> getDesiredRoomRankProbabilitiesFromJSON() throws IOException, ParseException {
-        EnumMap<HotelVisitPurpose,Map<RoomRank,Integer>> probabilities = new EnumMap<>(HotelVisitPurpose.class);
         JSONObject jsonObject = (JSONObject)((JSONObject) parser.parse(new FileReader(filePath))).get("desired_rank_probabilities");
-        for(HotelVisitPurpose hotelVisitPurpose : HotelVisitPurpose.values()){
-            JSONObject roomRankProbabilitiesJSONObject = (JSONObject) jsonObject.get(hotelVisitPurpose.toString());
-            EnumMap<RoomRank,Integer> roomRankProbabilities = new EnumMap<>(RoomRank.class);
-            for(RoomRank roomRank : RoomRank.values()){
-                int probability = ((Long) roomRankProbabilitiesJSONObject.get(roomRank.toString())).intValue();
-                roomRankProbabilities.put(roomRank,probability);
-            }
-            probabilities.put(hotelVisitPurpose,roomRankProbabilities);
-        }
-        return probabilities;
+         return Stream.of(HotelVisitPurpose.values()).collect(Collectors.toMap(
+                e->e,
+                e-> {
+                    JSONObject roomRankProbabilitiesJSONObject = (JSONObject) jsonObject.get(e.toString());
+                    return Stream.of(RoomRank.values()).collect(Collectors.toMap(
+                            r -> r,
+                            r -> ((Long) roomRankProbabilitiesJSONObject.get(r.toString())).intValue(),
+                            (a,b) -> b,
+                            () -> new EnumMap<>(RoomRank.class)
+                    ));
+                },
+                (a,b) -> b,
+                () -> new EnumMap<>(HotelVisitPurpose.class)
+        ));
     }
 
 
     public static EnumMap<HotelVisitPurpose,Map<Integer,Integer>> getNumberOfNightsProbabilitiesFromJSON() throws IOException, ParseException {
-        EnumMap<HotelVisitPurpose, Map<Integer,Integer>> probabilities = new EnumMap<>(HotelVisitPurpose.class);
-        JSONObject jsonObject = (JSONObject)((JSONObject) parser.parse(new FileReader(filePath))).get("number_of_nights_probabilities");
-        for(HotelVisitPurpose hotelVisitPurpose : HotelVisitPurpose.values()){
-            JSONArray roomSizeProbabilitiesJSONArray = (JSONArray) jsonObject.get(hotelVisitPurpose.toString());
-            Map<Integer,Integer> roomSizeProbabilities = new HashMap<>();
-            int roomSize = 1;
-            for (Object o : roomSizeProbabilitiesJSONArray) {
-                roomSizeProbabilities.put(roomSize++,((Long)o).intValue());
-            }
 
-            probabilities.put(hotelVisitPurpose,roomSizeProbabilities);
-        }
-        return probabilities;
+        JSONObject jsonObject = (JSONObject)((JSONObject) parser.parse(new FileReader(filePath))).get("number_of_nights_probabilities");
+        return Stream.of(HotelVisitPurpose.values()).collect(Collectors.toMap(
+                e->e,
+                e-> {
+                    JSONArray roomSizeProbabilitiesJSONArray = (JSONArray) jsonObject.get(e.toString());
+                    return IntStream.range(0, roomSizeProbabilitiesJSONArray.size())
+                            .boxed()
+                            .collect(Collectors.toMap(
+                                    i -> i + 1,
+                                    i -> ((Long) roomSizeProbabilitiesJSONArray.get(i)).intValue(),
+                                    (a, b) -> b,
+                                    HashMap::new
+                            ));
+                },
+                (a,b) -> b,
+                () -> new EnumMap<>(HotelVisitPurpose.class)
+        ));
     }
 
     public static EnumMap<HotelVisitPurpose,Map<Integer,Integer>> getRoomSizeProbabilitiesFromJSON() throws IOException, ParseException {
-        EnumMap<HotelVisitPurpose, Map<Integer,Integer>> probabilities = new EnumMap<>(HotelVisitPurpose.class);
-        JSONObject jsonObject = (JSONObject)((JSONObject) parser.parse(new FileReader(filePath))).get("room_size_probabilities");
-        for(HotelVisitPurpose hotelVisitPurpose : HotelVisitPurpose.values()){
-            JSONArray roomSizeProbabilitiesJSONArray = (JSONArray) jsonObject.get(hotelVisitPurpose.toString());
-            Map<Integer,Integer> roomSizeProbabilities = new HashMap<>();
-            int roomSize = 1;
-            for (Object o : roomSizeProbabilitiesJSONArray) {
-                roomSizeProbabilities.put(roomSize++,((Long)o).intValue());
-            }
 
-            probabilities.put(hotelVisitPurpose,roomSizeProbabilities);
-        }
-        return probabilities;
+        JSONObject jsonObject = (JSONObject)((JSONObject) parser.parse(new FileReader(filePath))).get("room_size_probabilities");
+        return Stream.of(HotelVisitPurpose.values()).collect(Collectors.toMap(
+                e->e,
+                e-> {
+                    JSONArray roomSizeProbabilitiesJSONArray = (JSONArray) jsonObject.get(e.toString());
+                    return IntStream.range(0, roomSizeProbabilitiesJSONArray.size())
+                            .boxed()
+                            .collect(Collectors.toMap(
+                                    i -> i + 1,
+                                    i -> ((Long) roomSizeProbabilitiesJSONArray.get(i)).intValue(),
+                                    (a, b) -> b,
+                                    HashMap::new
+                            ));
+                },
+                (a,b) -> b,
+                () -> new EnumMap<>(HotelVisitPurpose.class)
+        ));
     }
 
     public static HashMap<String, Long> getAttractivenessConstantsFromJSON() throws IOException, ParseException {
-        HashMap<String, Long> constants = new HashMap<>();
         JSONObject jsonObject = (JSONObject)((JSONObject) parser.parse(new FileReader(filePath))).get("attractiveness_constants");
 
-        for(Object constant: jsonObject.keySet()){
-            constants.put(String.valueOf(constant), (Long) jsonObject.get(constant));
-        }
-        return constants;
+        return Stream.of(jsonObject.keySet().toArray())
+                .collect(Collectors.toMap(
+                        e -> (String)e,
+                        e -> (Long)jsonObject.get(e),
+                        (a,b) -> b,
+                        HashMap::new
+                ));
     }
 
     public static EnumMap<RoomRank,Map<Integer,Integer>> getAveragePricesPerNightFromJSON() throws IOException, ParseException {
-        EnumMap<RoomRank, Map<Integer,Integer>> prices = new EnumMap<>(RoomRank.class);
+
         JSONObject jsonObject = (JSONObject)((JSONObject) parser.parse(new FileReader(filePath))).get("average_prices_per_nights");
-        for(RoomRank roomRank : RoomRank.values()){
-            JSONArray pricesJSONArray = (JSONArray) jsonObject.get(roomRank.toString());
-            Map<Integer,Integer> averagePricesPerNightForRoomRank = new HashMap<>();
-            int roomSize = 1;
-            for (Object o : pricesJSONArray) {
-                averagePricesPerNightForRoomRank.put(roomSize++,((Long)o).intValue());
-            }
-            prices.put(roomRank,averagePricesPerNightForRoomRank);
-        }
-        return prices;
+        return Stream.of(RoomRank.values()).collect(Collectors.toMap(
+                e->e,
+                e-> {
+                    JSONArray pricesJSONArray = (JSONArray) jsonObject.get(e.toString());
+                    return IntStream.range(0, pricesJSONArray.size())
+                            .boxed()
+                            .collect(Collectors.toMap(
+                                    i -> i + 1,
+                                    i -> ((Long) pricesJSONArray.get(i)).intValue(),
+                                    (a, b) -> b,
+                                    HashMap::new
+                            ));
+                },
+                (a,b) -> b,
+                () -> new EnumMap<>(RoomRank.class)
+        ));
     }
 
     public static EnumMap<SingleAdvertisementType, SingleAdvertisementData> getSingleAdvertisementDataFromJSON() throws IOException, ParseException{
-        EnumMap<SingleAdvertisementType,SingleAdvertisementData> singleAdvertisementData = new EnumMap<>(SingleAdvertisementType.class);
+
         JSONObject jsonObject = (JSONObject)((JSONObject) parser.parse(new FileReader(filePath))).get("single_advertisement");
-        for(SingleAdvertisementType type : SingleAdvertisementType.values()){
-            EnumMap<HotelVisitPurpose,Double> effectiveness = new EnumMap<>(HotelVisitPurpose.class);
-            JSONObject data = (JSONObject) jsonObject.get(type.toString());
-            JSONObject effectivenessData = (JSONObject) data.get("effectiveness");
-            for(HotelVisitPurpose hotelVisitPurpose : HotelVisitPurpose.values()){
-                effectiveness.put(hotelVisitPurpose, ((Long)effectivenessData.get(hotelVisitPurpose.toString())).doubleValue() / 100);
-            }
-            BigDecimal costOfPurchase = BigDecimal.valueOf(((Long)data.get("cost_of_purchase")).doubleValue());
 
+        return Stream.of(SingleAdvertisementType.values()).collect(Collectors.toMap(
+                e -> e,
+                e -> {
+                    JSONObject data = (JSONObject) jsonObject.get(e.toString());
+                    JSONObject effectivenessData = (JSONObject) data.get("effectiveness");
+                    return  new SingleAdvertisementData(
+                            BigDecimal.valueOf(((Long)data.get("cost_of_purchase")).doubleValue()),
+                            Stream.of(HotelVisitPurpose.values()).collect(Collectors.toMap(
+                                    h -> h,
+                                    h -> (Double)effectivenessData.get(h.toString()),
+                                    (a,b) -> b,
+                                    () -> new EnumMap<>(HotelVisitPurpose.class)
+                            )));
+                },
+                (a,b) -> a,
+                () -> new EnumMap<>(SingleAdvertisementType.class)
 
-            singleAdvertisementData.put(type,new SingleAdvertisementData(costOfPurchase,effectiveness));
-        }
-        return singleAdvertisementData;
+        ));
     }
 
     public static EnumMap<ConstantAdvertisementType, ConstantAdvertisementData> getConstantAdvertisementDataFromJSON() throws IOException, ParseException{
-        EnumMap<ConstantAdvertisementType,ConstantAdvertisementData> constantAdvertisementData = new EnumMap<>(ConstantAdvertisementType.class);
         JSONObject jsonObject = (JSONObject)((JSONObject) parser.parse(new FileReader(filePath))).get("constant_advertisement");
-        for(ConstantAdvertisementType type : ConstantAdvertisementType.values()){
-            EnumMap<HotelVisitPurpose,Double> effectiveness = new EnumMap<>(HotelVisitPurpose.class);
-            JSONObject data = (JSONObject) jsonObject.get(type.toString());
-            JSONObject effectivenessData = (JSONObject) data.get("effectiveness");
-            for(HotelVisitPurpose hotelVisitPurpose : HotelVisitPurpose.values()){
-                effectiveness.put(hotelVisitPurpose, ((Long)effectivenessData.get(hotelVisitPurpose.toString())).doubleValue() / 100);
-            }
-            BigDecimal costOfPurchase = BigDecimal.valueOf(((Long)data.get("cost_of_purchase")).doubleValue());
-            BigDecimal costOfMaintenance = BigDecimal.valueOf(((Long)data.get("cost_of_maintenance")).doubleValue());
-            constantAdvertisementData.put(type,new ConstantAdvertisementData(costOfPurchase,costOfMaintenance,effectiveness));
-        }
-        return constantAdvertisementData;
+
+         return Stream.of(ConstantAdvertisementType.values()).collect(Collectors.toMap(
+                e -> e,
+                e -> {
+                    JSONObject data = (JSONObject) jsonObject.get(e.toString());
+                    JSONObject effectivenessData = (JSONObject) data.get("effectiveness");
+                    return  new ConstantAdvertisementData(
+                            BigDecimal.valueOf(((Long)data.get("cost_of_purchase")).doubleValue()),
+                            BigDecimal.valueOf(((Long)data.get("cost_of_maintenance")).doubleValue()),
+                            Stream.of(HotelVisitPurpose.values()).collect(Collectors.toMap(
+                                    h -> h,
+                                    h -> (Double)effectivenessData.get(h.toString()),
+                                    (a,b) -> b,
+                                    () -> new EnumMap<>(HotelVisitPurpose.class)
+                            )));
+                },
+                 (a,b) -> a,
+                 () -> new EnumMap<>(ConstantAdvertisementType.class)
+
+        ));
     }
 
     public static void main(String[] args) throws IOException, ParseException {
