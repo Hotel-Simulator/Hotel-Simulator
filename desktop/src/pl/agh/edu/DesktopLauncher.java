f62637e0c23be12524c89abfa46f4710f5b2d3c6
pwgacek <pawelgacek@student.agh.edu.pl>
2023-07-24 21:27:43 +0200

    removed unnecessary println

--- desktop/src/pl/agh/edu/DesktopLauncher.java
+++ desktop/src/pl/agh/edu/DesktopLauncher.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		System.out.println(System.getProperty("user.dir"));
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Hotel simulator");
