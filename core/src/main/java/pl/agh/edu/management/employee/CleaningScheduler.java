13f79fee54e1b0b9e5c376b387fef7706f4b744e
Paweł Gacek <80721230+pwgacek@users.noreply.github.com>
2023-08-09 17:00:44 +0200

    added RepairScheduler & now there is a possibility that room will become faulty (#6)

--- core/src/main/java/pl/agh/edu/management/employee/CleaningScheduler.java
+++ core/src/main/java/pl/agh/edu/management/employee/CleaningScheduler.java
@@ -5,74 +5,33 @@ import pl.agh.edu.model.Hotel;
 import pl.agh.edu.model.Room;
 import pl.agh.edu.model.employee.Employee;
 import pl.agh.edu.model.employee.Profession;
-import pl.agh.edu.model.employee.Shift;
-import pl.agh.edu.model.time.Time;
 import pl.agh.edu.time_command.TimeCommand;
-import pl.agh.edu.time_command.TimeCommandExecutor;
-
 import java.util.*;
-import java.util.stream.Collectors;
 
-public class CleaningScheduler {
-    private List<Employee> cleaners;
-    private final Hotel hotel;
-    private final PriorityQueue<Room> dirtyRooms;
-    private final TimeCommandExecutor timeCommandExecutor;
-    private Shift currentShift;
-    private final Time time;
+public class CleaningScheduler extends WorkScheduler {
     public CleaningScheduler(Hotel hotel){
-        this.hotel = hotel;
-        this.dirtyRooms = new PriorityQueue<>(roomComparator);
-        currentShift = Shift.NIGHT;
-        this.timeCommandExecutor = TimeCommandExecutor.getInstance();
-        this.time = Time.getInstance();
-        this.cleaners = new ArrayList<>();
-    }
-
-    public void newDirtyRoom(Room room){
-        dirtyRooms.add(room);
-        if(dirtyRooms.size() == 1){
-            cleaners.stream()
-                    .filter(cleaner -> !cleaner.isOccupied() && willEmployeeExecuteServiceBeforeShiftEnds(cleaner))
-                    .findFirst()
-                    .ifPresent(cleaner -> cleanRoom(cleaner,dirtyRooms.remove()));
-        }
-    }
-
-    public void perShiftUpdate(){
-        currentShift = currentShift.next();
-        cleaners = hotel.getWorkingEmployeesByProfession(Profession.CLEANER).stream()
-                .filter(cleaner -> cleaner.getShift().equals(currentShift))
-                .collect(Collectors.toList());
-        cleaners.forEach(this::cleanRoomIfPossible);
-
+        super(hotel, new PriorityQueue<>(roomComparator),Profession.CLEANER);
     }
 
     public void dailyAtCheckOutTimeUpdate(){
-        int sizeBefore = dirtyRooms.size();
-        dirtyRooms.addAll(hotel.getRooms().stream()
+        int sizeBefore = roomsToExecuteService.size();
+        roomsToExecuteService.addAll(hotel.getRooms().stream()
                 .filter(room -> room.getState() == RoomState.OCCUPIED)
                 .toList());
-        if(sizeBefore == 0 && !dirtyRooms.isEmpty()){
-            cleaners.stream()
+        if(sizeBefore == 0 && !roomsToExecuteService.isEmpty()){
+            workingEmployees.stream()
                     .filter(cleaner -> !cleaner.isOccupied())
-                    .forEach(this::cleanRoomIfPossible);
+                    .forEach(this::executeServiceIfPossible);
         }
     }
 
     public void dailyAtCheckInTimeUpdate(){
         //todo tutaj Bartek
-        dirtyRooms.removeIf(room -> room.getState() == RoomState.OCCUPIED);
+        roomsToExecuteService.removeIf(room -> room.getState() == RoomState.OCCUPIED);
     }
 
-
-    public void cleanRoomIfPossible(Employee cleaner){
-
-        if(!dirtyRooms.isEmpty() && willEmployeeExecuteServiceBeforeShiftEnds(cleaner)){
-            cleanRoom(cleaner,dirtyRooms.remove());
-        }
-    }
-    private void cleanRoom(Employee cleaner, Room room){
+    @Override
+    protected void executeService(Employee cleaner, Room room){
         cleaner.setOccupied(true);
         if(room.getState() == RoomState.DIRTY) room.setState(RoomState.MAINTENANCE);
         else if(room.getState() == RoomState.OCCUPIED) room.setState(RoomState.OCCUPIED_MAINTENANCE);
@@ -81,13 +40,10 @@ public class CleaningScheduler {
                     cleaner.setOccupied(false);
                     if(room.getState() == RoomState.MAINTENANCE) room.setState(RoomState.EMPTY);
                     else if(room.getState() == RoomState.OCCUPIED_MAINTENANCE) room.setState(RoomState.OCCUPIED);
-                    cleanRoomIfPossible(cleaner);
+                    executeServiceIfPossible(cleaner);
                 }));
     }
 
-    private boolean willEmployeeExecuteServiceBeforeShiftEnds(Employee cleaner){
-        return cleaner.isAtWork(time.getTime().toLocalTime().plusMinutes(cleaner.getServiceExecutionTime().toMinutes()));
-    }
 
     private static final Comparator<Room> roomComparator = (o1, o2) -> {
         if(o1.getState() == o2.getState()) return 0;
---
+++ core/src/main/java/pl/agh/edu/management/employee/RepairScheduler.java
@@ -0,0 +1,28 @@
+package pl.agh.edu.management.employee;
+
+import pl.agh.edu.enums.RoomState;
+import pl.agh.edu.model.Hotel;
+import pl.agh.edu.model.Room;
+import pl.agh.edu.model.employee.Employee;
+import pl.agh.edu.model.employee.Profession;
+import pl.agh.edu.time_command.TimeCommand;
+import java.util.LinkedList;
+
+
+public class RepairScheduler extends WorkScheduler {
+    public RepairScheduler(Hotel hotel){
+        super(hotel, new LinkedList<>(),Profession.CLEANER);
+    }
+    @Override
+    protected void executeService(Employee technician, Room room){
+        technician.setOccupied(true);
+        room.setState(RoomState.MAINTENANCE); //todo tutaj zmienic na isBeingRepaired = true;
+        timeCommandExecutor.addCommand(time.getTime().plusMinutes(technician.getServiceExecutionTime().toMinutes()),
+                new TimeCommand(() ->{
+                    technician.setOccupied(false);
+                    room.setState(RoomState.EMPTY); //todo tutaj ustawic ze jest naprawiony
+                    executeServiceIfPossible(technician);
+                }));
+    }
+
+}
