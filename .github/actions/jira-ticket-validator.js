cf7f6370b1ded3665fd29af6035c4a0d65574b58
Jakub Spólnik <jakubspolnik@student.agh.edu.pl>
2023-08-14 23:18:33 +0200

    HS-150 | Fix validation for PR title

--- .github/actions/jira-ticket-validator.js
+++ .github/actions/jira-ticket-validator.js
@@ -12,7 +12,7 @@ function validateJiraTicket(jiraTicketRegex, input, fieldName) {
 const jiraTicketRegex = /^HS-\d+(\s\|\s\w.*)?$/;
 
 const branchName = process.env.GITHUB_HEAD_REF || '';
-const prTitle = process.env.prTitle || '';
+const prTitle = process.argv[2];
 
 console.log(`Validating branch name: ${branchName}`);
 validateJiraTicket(jiraTicketRegex, branchName, 'Branch name');
