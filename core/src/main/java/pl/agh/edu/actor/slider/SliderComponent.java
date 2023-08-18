65c8172c252770fbae83757dd1a6b597c521110b
jagodek <godek.main@gmail.com>
2023-08-18 15:01:28 +0200

    fixed some issues e.g. reformated font, padding; skin update with backgrounds

--- core/src/main/java/pl/agh/edu/actor/slider/SliderComponent.java
+++ core/src/main/java/pl/agh/edu/actor/slider/SliderComponent.java
@@ -1,45 +1,51 @@
 package pl.agh.edu.actor.slider;
 
-import java.util.function.Function;
-
 import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.utils.Align;
 
 import pl.agh.edu.actor.HotelSkin;
 
 public class SliderComponent extends Table {
-	private int SLIDER_WIDTH = 600;
-	private int HORIZONTAL_PADDING = 20;
+	private final float SLIDER_WIDTH = 600f;
+	private final float SLIDER_HEIGHT = 60f;
+	private final float HORIZONTAL_OUTER_PADDING = 50f;
+	private final float HORIZONTAL_INNER_PADDING = 30f;
 	private Label valueLabel;
 	private Slider slider;
 	private Skin skin;
 	private String suffix;
-	private Function<Float, Float> mapping;
 
-	public SliderComponent(String name, String suffix, float min, float max, float step, Function<Float, Float> mapping) {
+	public SliderComponent(String name, String suffix, float min, float max, float step) {
 		Stack stack = new Stack();
-		add(stack);
+		add(stack).height(SLIDER_HEIGHT);
 		skin = HotelSkin.getInstance();
-		NinePatch texture = skin.getPatch("textfield-background");
+		NinePatch texture = skin.getPatch("gray-100-border-round");
 		stack.add(new Image(texture));
-		this.mapping = mapping;
 		this.suffix = suffix;
 		stack.add(new SliderRowTable(name, min, max, step));
+
 	}
 
-	public class SliderRowTable extends Table {
+	private class SliderRowTable extends Table {
 
 		public SliderRowTable(String name, float min, float max, float step) {
-			Label nameLabel = new Label(name + ":", skin);
-			nameLabel.setDebug(true);
-			add(nameLabel).width(SLIDER_WIDTH / 6).padLeft(HORIZONTAL_PADDING);
+			Label nameLabel = new Label(name + ":", skin, "subtitle1_label");
+			add(nameLabel).minWidth(SLIDER_WIDTH / 4).padLeft(HORIZONTAL_OUTER_PADDING);
 
-			valueLabel = new Label("", skin);
+			valueLabel = new Label("", skin, "subtitle1_label");
 			valueLabel.setAlignment(Align.right);
-			add(valueLabel).width(SLIDER_WIDTH / 4).padRight(HORIZONTAL_PADDING);
+			valueLabel.setText(String.format("%.0f", max) + suffix);
+			add(valueLabel).minWidth(SLIDER_WIDTH / 4);
+
+			Drawable drawable = skin.getDrawable("gray-500-borderless");
+			drawable.setMinHeight(SLIDER_HEIGHT);
+			drawable.setMinWidth(2);
+			Image separator = new Image(drawable);
+			add(separator).bottom().expandY().padLeft(HORIZONTAL_INNER_PADDING).padRight(HORIZONTAL_INNER_PADDING);
 
 			slider = new Slider(min, max, step, false, skin);
 			slider.addListener(new ChangeListener() {
@@ -48,22 +54,18 @@ public class SliderComponent extends Table {
 					setField();
 				}
 			});
-			add(slider).width(SLIDER_WIDTH / 3).padRight(HORIZONTAL_PADDING);
+			add(slider).width(SLIDER_WIDTH / 3).padRight(HORIZONTAL_OUTER_PADDING);
 
 			setField();
 		}
 
 		private void setField() {
-			Float value = mapping.apply(slider.getValue());
-			if (value - value.intValue() == 0)
-				valueLabel.setText(value.intValue() + suffix);
-			else
-				valueLabel.setText(Math.round(value * 10) / 10 + suffix);
+			valueLabel.setText(String.format("%.0f", getValue()) + suffix);
 		}
 	}
 
 	public Float getValue() {
-		return mapping.apply(slider.getValue());
+		return slider.getValue();
 	}
 
 }
