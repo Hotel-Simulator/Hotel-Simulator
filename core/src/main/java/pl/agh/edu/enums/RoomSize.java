082eaa5129dc63612fbb967b225a8222bff8e3d6
Paweł Gacek <pawelgacek@student.agh.edu.pl>
2023-09-10 09:47:58 +0200

    HS-180 [Review]

--- core/src/main/java/pl/agh/edu/enums/RoomSize.java
+++ core/src/main/java/pl/agh/edu/enums/RoomSize.java
@@ -1,6 +1,5 @@
 package pl.agh.edu.enums;
 
-import java.util.Arrays;
 import java.util.Optional;
 
 public enum RoomSize {
@@ -18,10 +17,13 @@ public enum RoomSize {
 		return maxNumberOfGuests >= numberOfGuests && numberOfGuests > 0;
 	}
 
-	public static Optional<RoomSize> getSmallestAvailableRoomSize(int clientGroupSize) {
-		return Arrays.stream(RoomSize.values())
-				.filter(roomSize -> roomSize.canAccommodateGuests(clientGroupSize))
-				.min(RoomSize::compareTo);
+	public static Optional<RoomSize> getSmallestAvailableRoomSize(int numberOfGuests) {
+		return switch (numberOfGuests) {
+		case 1 -> Optional.of(RoomSize.SINGLE);
+		case 2 -> Optional.of(RoomSize.DOUBLE);
+		case 3, 4, 5 -> Optional.of(RoomSize.FAMILY);
+		default -> Optional.empty();
+		};
 	}
 
 }
