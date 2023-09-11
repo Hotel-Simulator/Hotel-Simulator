e3b3feae5ac5f4db4e0ceb18ce629c5e4168fb9d
jagodek <godek.main@gmail.com>
2023-09-11 17:38:12 +0200

    HS-142 | dedicated background for valuetag

---
+++ core/src/main/java/pl/agh/edu/actor/component/TextComponents/ValueTag.java
@@ -0,0 +1,82 @@
+package pl.agh.edu.actor.component;
+
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.NinePatch;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.utils.Align;
+import pl.agh.edu.actor.HotelSkin;
+
+public class ValueTag extends Table {
+    private String Tag;
+    private String Value;
+    private Skin skin;
+
+    private final float WIDTH;
+    private final float HEIGHT;
+    private final float HORIZONTAL_PADDING;
+
+    public ValueTag(String tag, String value, ValueTagSize size) {
+        WIDTH = size.getWIDTH();
+        HEIGHT = size.getHEIGHT();
+        HORIZONTAL_PADDING = size.getHORIZONTAL_PADDING();
+        System.out.println("HEIGHT = " + HEIGHT);
+        System.out.println("WIDTH = " + WIDTH);
+        Stack componentStack = new Stack();
+        add(componentStack).height(HEIGHT).width(WIDTH);
+        skin = HotelSkin.getInstance();
+        // TODO: 11.09.2023 add enw background to skin  
+        NinePatch background = skin.getPatch("slider-component-background");
+        componentStack.add(new Image(background));
+        componentStack.add(new ValueTagContent(tag, value));
+    }
+
+    private class ValueTagContent extends Table{
+        ValueTagContent(String tag, String value){
+            setDebug(true);
+            this.padLeft(HORIZONTAL_PADDING).padRight(HORIZONTAL_PADDING);
+
+            Label tagLabel = new Label(tag,skin, "body1_label");
+            tagLabel.setAlignment(Align.left);
+            this.add(tagLabel).growX();
+
+
+            Label valueLabel = new Label(value,skin, "body1_label");
+
+            this.add(valueLabel);
+
+        }
+    }
+
+    public enum ValueTagSize{
+        SMALL(500f,40f,40f),
+        MEDIUM(550f,40f,40f),
+        LARGE(600f,40f,40f);
+
+        private final float WIDTH;
+        private final float HEIGHT;
+        private final float HORIZONTAL_PADDING;
+
+        ValueTagSize(float WIDTH, float HEIGHT, float HORIZONTAL_PADDING) {
+            this.WIDTH = WIDTH;
+            this.HEIGHT = HEIGHT;
+            this.HORIZONTAL_PADDING = HORIZONTAL_PADDING;
+
+        }
+
+
+        public float getWIDTH() {
+            return WIDTH;
+        }
+
+        public float getHEIGHT() {
+            return HEIGHT;
+        }
+
+        public float getHORIZONTAL_PADDING() {
+            return HORIZONTAL_PADDING;
+        }
+
+
+    }
+
+}
--- core/src/main/java/pl/agh/edu/actor/frame/TestFrame.java
+++ core/src/main/java/pl/agh/edu/actor/frame/TestFrame.java
@@ -1,39 +1,17 @@
 package pl.agh.edu.actor.frame;
 
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.utils.Array;
+import java.math.BigDecimal;
 
-import pl.agh.edu.actor.HotelSkin;
-import pl.agh.edu.actor.component.selectMenu.SelectMenu;
-import pl.agh.edu.actor.component.selectMenu.SelectMenuItem;
-import pl.agh.edu.actor.component.selectMenu.SelectedMenuStringItem;
-import pl.agh.edu.actor.utils.Size;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
+import pl.agh.edu.actor.component.ValueTag;
 
 public class TestFrame extends BaseFrame {
 	public TestFrame(String name) {
 		super();
-		this.add(new Label(name, HotelSkin.getInstance()));
+		Table root = new Table();
+		root.add(new ValueTag("Deposit interest rate","7%", ValueTag.ValueTagSize.LARGE));
 
-		Array<SelectMenuItem> items = new Array<>();
-		items.add(new SelectedMenuStringItem("Option 1 Example Example Example", Size.SMALL));
-		items.add(new SelectedMenuStringItem("Option 2", Size.SMALL));
-		items.add(new SelectedMenuStringItem("Option 3", Size.SMALL));
-		items.add(new SelectedMenuStringItem("Option 4", Size.SMALL));
-		items.add(new SelectedMenuStringItem("Option 5", Size.SMALL));
-		items.add(new SelectedMenuStringItem("Option 6", Size.SMALL));
-		items.add(new SelectedMenuStringItem("Option 7", Size.SMALL));
-		items.add(new SelectedMenuStringItem("Option 8", Size.SMALL));
-		items.add(new SelectedMenuStringItem("Option 9", Size.SMALL));
-
-		SelectMenu selectMenu = new SelectMenu(
-				Size.SMALL,
-				"Example",
-				items,
-				selectedOption -> {
-					System.out.println("Selected: " + selectedOption);
-					return null;
-				});
-
-		this.add(selectMenu);
+		this.add(root);
 	}
 }
