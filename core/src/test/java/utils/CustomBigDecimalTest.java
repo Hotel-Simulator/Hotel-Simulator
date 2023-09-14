a2fdd57551451a2528c4cef58495c44bc1817c1c
jagodek <godek.main@gmail.com>
2023-09-14 16:01:52 +0200

    HS-142 | modification of tests

--- core/src/test/java/utils/CustomBigDecimalTest.java
+++ core/src/test/java/utils/CustomBigDecimalTest.java
@@ -1,23 +1,27 @@
 package utils;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import pl.agh.edu.utils.CustomBigDecimal;
 
-import java.io.File;
 import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CustomBigDecimalTest {
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/utils/customBigDecimalToString.csv")
-    public void testToString(String value, String printed){
-        assertEquals(printed, new CustomBigDecimal(value).toString());
+    @CsvFileSource(resources = "/utils/customBigDecimal/toString.csv")
+    public void testToString(String value, String expected){
+        assertEquals(expected, new CustomBigDecimal(value).toString());
     }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/utils/customBigDecimal/roundToStringValue.csv")
+    public void testRounding(String value, String expected){
+        BigDecimal actual = new CustomBigDecimal(value).roundToStringValue().getValue();
+        assertEquals(new BigDecimal(expected), actual);
+    }
+
 
 }
