e27f87756e6eea0ee920511327873dbfc0d9a753
jagodek <godek.main@gmail.com>
2023-09-27 21:14:23 +0200

    HS-142 | ValueTag

--- core/src/main/java/pl/agh/edu/actor/TextActors/ValueTag.java
+++ core/src/main/java/pl/agh/edu/actor/TextActors/ValueTag.java
@@ -1,64 +1,95 @@
 package pl.agh.edu.actor.TextActors;
 
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.utils.Align;
+
 import pl.agh.edu.actor.HotelSkin;
-import pl.agh.edu.actor.utils.Size;
 import pl.agh.edu.config.GraphicConfig;
+import pl.agh.edu.language.LanguageChangeListener;
+import pl.agh.edu.language.LanguageManager;
 
-public class ValueTag extends Table {
-    private String Tag;
-    private String Value;
-    private final Skin skin;
-    private static Size size = Size.SMALL;
+public class ValueTag extends Table implements LanguageChangeListener {
+	private final Skin skin;
+	private final String languagePath;
+	private Label tagLabel;
+	private Label valueLabel;
 
+	public ValueTag(String languagePath, String value) {
+		this.languagePath = languagePath;
+		this.skin = HotelSkin.getInstance();
+		Stack componentStack = new Stack();
 
-    public ValueTag(String tag, String value) {
-        Stack componentStack = new Stack();
-        add(componentStack).height(ValueTagStyle.getHeight()).width(ValueTagStyle.getWidth());
-        skin = HotelSkin.getInstance();
-        NinePatch background = skin.getPatch("valuetag-background");
-        componentStack.add(new Image(background));
-        componentStack.add(new ValueTagContent(tag, value));
-    }
+		add(componentStack).height(ValueTagStyle.getHeight()).width(ValueTagStyle.getWidth());
+		NinePatch background = skin.getPatch("valuetag-background");
+		componentStack.add(new Image(background));
+		componentStack.add(new ValueTagContent(languagePath, value));
+		LanguageManager.addListener(this);
 
-    private class ValueTagContent extends Table{
-        ValueTagContent(String tag, String value){
-            this.padLeft(ValueTagStyle.getPadding()).padRight(ValueTagStyle.getPadding());
+	}
 
-            Label tagLabel = new Label(tag,skin,"subtitle1_label");
-            this.add(tagLabel).growX();
+	private class ValueTagContent extends Table {
+		ValueTagContent(String tag, String value) {
 
-            Label valueLabel = new Label(value,skin,"white_subtitle1_label");
-            valueLabel.setColor(skin.getColor("Gray_700"));
+			this.padLeft(ValueTagStyle.getPadding()).padRight(ValueTagStyle.getPadding());
 
-        }
-    }
+			tagLabel = new Label(tag, skin, "subtitle1_label");
+			this.add(tagLabel).growX();
 
-    private static class ValueTagStyle {
-        public static float getHeight() {
-            return switch (size) {
-                case SMALL -> 60f;
-                case MEDIUM -> 70f;
-                case LARGE -> 80f;
-            };
-        }
-        public static float getWidth() {
-            return switch (size) {
-                case SMALL -> 250f;
-                case MEDIUM -> 270f;
-                case LARGE -> 300f;
-            };
-        }
-        public static float getPadding() {
-            return switch (size) {
-                case SMALL -> 250f;
-                case MEDIUM -> 270f;
-                case LARGE -> 300f;
-            };
-        }
+			valueLabel = new Label(value, skin, "white_subtitle1_label");
+			valueLabel.setColor(skin.getColor("Gray_700"));
+			valueLabel.setAlignment(Align.right);
+			this.add(valueLabel).growX();
+		}
+	}
 
-    }
+	public void setTagColor(Color color) {
+		setColor(tagLabel, color);
+	}
+
+	public void setValueColor(Color color) {
+		setColor(valueLabel, color);
+	}
+
+	private void setColor(Label coloredLabel, Color color) {
+		Label.LabelStyle labelStyle = coloredLabel.getStyle();
+		String labelName = labelStyle.font.toString();
+		labelStyle.font = skin.getFont(labelName.startsWith("white-") ? labelName : "white-" + labelName);
+		coloredLabel.setStyle(labelStyle);
+		coloredLabel.setColor(color);
+	}
+
+	@Override
+	public void onLanguageChange() {
+		this.tagLabel.setText(LanguageManager.get(languagePath));
+	}
+
+	private static class ValueTagStyle {
+		public static float getHeight() {
+			return switch (GraphicConfig.getResolution().SIZE) {
+				case SMALL -> 40f;
+				case MEDIUM -> 50f;
+				case LARGE -> 60f;
+			};
+		}
+
+		public static float getWidth() {
+			return switch (GraphicConfig.getResolution().SIZE) {
+				case SMALL -> 400f;
+				case MEDIUM -> 600f;
+				case LARGE -> 900f;
+			};
+		}
+
+		public static float getPadding() {
+			return switch (GraphicConfig.getResolution().SIZE) {
+				case SMALL -> 20f;
+				case MEDIUM -> 30f;
+				case LARGE -> 40f;
+			};
+		}
+
+	}
 
 }
