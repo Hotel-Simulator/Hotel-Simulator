16bfb80fc0cf2eed66576136dcb0821dcfd3a861
Paweł Gacek <80721230+pwgacek@users.noreply.github.com>
2023-10-06 17:55:48 +0200

    HS-204 | Refactor workingDir (#51)
    * HS-202 Refactor workingDir
    
    * HS-204 [Review]
    
    * HS-204 [Review]
    
    * HS-204 fix after merge
    
    * HS-204 fix after merge

--- core/src/test/java/management/HotelScenariosManagerTest.java
+++ core/src/test/java/management/HotelScenariosManagerTest.java
@@ -2,23 +2,14 @@ package management;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.lang.reflect.Field;
-
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import pl.agh.edu.enums.HotelType;
-import pl.agh.edu.json.data_extractor.JSONFilePath;
 import pl.agh.edu.management.hotel.HotelScenariosManager;
 
 public class HotelScenariosManagerTest {
 	private static HotelScenariosManager hotelScenariosManager;
-
-	@BeforeAll
-	public static void setUpClass() throws ReflectiveOperationException {
-		changeJSONPath();
-	}
 
 	@BeforeEach
 	public void setUp() {
@@ -50,12 +41,5 @@ public class HotelScenariosManagerTest {
 	public void testHotelScenariosManagerInitialization() {
 		// Then
 		assertNotNull(hotelScenariosManager.getHotelVisitPurposeProbabilities());
-	}
-
-	private static void changeJSONPath()
-			throws ReflectiveOperationException {
-		Field field = JSONFilePath.class.getDeclaredField("PATH");
-		field.setAccessible(true);
-		field.set(null, "../assets/jsons/%s.json");
 	}
 }
