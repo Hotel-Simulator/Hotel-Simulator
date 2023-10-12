de1003a212d97e2cbc96c2a14f7dc70b053f33b5
jagodek <godek.main@gmail.com>
2023-10-12 23:01:27 +0200

    HS-140 | spotlessappl

--- core/src/main/java/pl/agh/edu/actor/utils/LanguageLabel.java
+++ core/src/main/java/pl/agh/edu/actor/utils/LanguageLabel.java
@@ -1,23 +1,24 @@
 package pl.agh.edu.actor.utils;
 
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+
 import pl.agh.edu.actor.HotelSkin;
 import pl.agh.edu.language.LanguageChangeListener;
 import pl.agh.edu.language.LanguageManager;
 
 public class LanguageLabel extends Label implements LanguageChangeListener {
-    private final String languagePath;
+	private final String languagePath;
 
-    public LanguageLabel(String languagePath, String font) {
-        super("", HotelSkin.getInstance(), font);
-        this.languagePath = languagePath;
-        LanguageManager.addListener(this);
-        onLanguageChange();
-    }
+	public LanguageLabel(String languagePath, String font) {
+		super("", HotelSkin.getInstance(), font);
+		this.languagePath = languagePath;
+		LanguageManager.addListener(this);
+		onLanguageChange();
+	}
 
-    @Override
-    public void onLanguageChange() {
-        this.setText(LanguageManager.get(languagePath));
-    }
+	@Override
+	public void onLanguageChange() {
+		this.setText(LanguageManager.get(languagePath));
+	}
 
 }
