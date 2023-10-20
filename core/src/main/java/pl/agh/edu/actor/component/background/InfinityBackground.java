dd7724cb7e3b374f114c94876f2da6cfad10dc77
Jakub Spólnik <jakubspolnik@student.agh.edu.pl>
2023-10-21 00:12:15 +0200

    HS-220 | Add accelerating background

--- core/src/main/java/pl/agh/edu/actor/component/background/InfinityBackground.java
+++ core/src/main/java/pl/agh/edu/actor/component/background/InfinityBackground.java
@@ -14,6 +14,7 @@ import pl.agh.edu.model.time.Time;
 public class InfinityBackground extends Stack implements ResolutionChangeListener {
 	private final Image firstBackground;
 	private final Image secondBackground;
+	private final Time time = Time.getInstance();
 
 	public InfinityBackground(String fileName) {
 		this.setFillParent(true);
@@ -31,7 +32,7 @@ public class InfinityBackground extends Stack implements ResolutionChangeListene
 						Actions.forever(
 								Actions.sequence(
 										Actions.run(() -> {
-											float movingDistance = 10.2f;
+											float movingDistance = 0.5f * time.getAcceleration();
 											if (image.getX() + GraphicConfig.getResolution().WIDTH <= 0) {
 												image.moveBy(-movingDistance + 2 * GraphicConfig.getResolution().WIDTH, 0f);
 											}
