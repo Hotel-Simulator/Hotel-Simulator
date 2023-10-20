5e20333d40523b6f27e011e29142da0de3f313a9
jagodek <godek.main@gmail.com>
2023-10-20 15:47:14 +0200

    fixup! HS-140 | fonts util class - white fonts added

--- core/src/main/java/pl/agh/edu/actor/utils/Fonts.java
+++ core/src/main/java/pl/agh/edu/actor/utils/Fonts.java
@@ -18,4 +18,16 @@ public class Fonts {
     public static final BitmapFont BUTTON2 = skin.getFont("button2");
     public static final BitmapFont BUTTON3 = skin.getFont("button3");
 
+    public static final BitmapFont WHITE_BODY1 = skin.getFont("white-body1");
+    public static final BitmapFont WHITE_BODY2 = skin.getFont("white-body2");
+    public static final BitmapFont WHITE_H1 = skin.getFont("white-h1");
+    public static final BitmapFont WHITE_H2 = skin.getFont("white-h2");
+    public static final BitmapFont WHITE_H3 = skin.getFont("white-h3");
+    public static final BitmapFont WHITE_H4 = skin.getFont("white-h4");
+    public static final BitmapFont WHITE_SUBTITLE1 = skin.getFont("white-subtitle1");
+    public static final BitmapFont WHITE_SUBTITLE2 = skin.getFont("white-subtitle2");
+    public static final BitmapFont WHITE_BUTTON1 = skin.getFont("white-button1");
+    public static final BitmapFont WHITE_BUTTON2 = skin.getFont("white-button2");
+    public static final BitmapFont WHITE_BUTTON3 = skin.getFont("white-button3");
+
 }
