e2a87e5f7da7739af020f859b022eccc5bf2e891
Jakub Spólnik <jakubspolnik@student.agh.edu.pl>
2023-10-21 00:13:03 +0200

    HS-221 | Linter fix

--- core/src/main/java/pl/agh/edu/actor/component/panel/navbar/MoneyPanel.java
+++ core/src/main/java/pl/agh/edu/actor/component/panel/navbar/MoneyPanel.java
@@ -1,16 +1,15 @@
 package pl.agh.edu.actor.component.panel.navbar;
 
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.Align;
 
-import pl.agh.edu.actor.GameSkin;
 import pl.agh.edu.actor.utils.CustomLabel;
 import pl.agh.edu.actor.utils.FontType;
 import pl.agh.edu.actor.utils.wrapper.WrapperTable;
 
 public class MoneyPanel extends WrapperTable {
 	private final Label moneyLabel = new MoneyLabel();
+
 	public MoneyPanel() {
 		innerTable.add(moneyLabel).grow().center();
 		this.size(220, 80);
