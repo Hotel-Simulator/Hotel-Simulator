4e8037d40c45ed2ac2899f0a95930dc19b04aaed
Michał Godek <mgodek@student.agh.edu.pl>
2023-10-21 18:42:39 +0200

    HS-225 | LanguageLabel, CustomLabel modified (#70)
    * HS-225 | LanguageLabel, CustomLabel modified
    
    * HS-225 | fix for font in CustomLabel
    
    * HS-225 | spottless
    
    * HS-225 | fix
    
    * HS-225 | spotless
    
    * HS-225 | fix
    
    * HS-225 | underline works
    
    * HS-225 | requested changes

--- core/src/main/java/pl/agh/edu/actor/frame/TestFrame.java
+++ core/src/main/java/pl/agh/edu/actor/frame/TestFrame.java
@@ -2,17 +2,23 @@ package pl.agh.edu.actor.frame;
 
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
-import pl.agh.edu.actor.component.button.LabeledButton;
-import pl.agh.edu.actor.utils.resolution.Size;
+import pl.agh.edu.actor.utils.CustomLabel;
+import pl.agh.edu.actor.utils.SkinColor;
 
 public class TestFrame extends BaseFrame {
 	public TestFrame(String name) {
 		super();
 		Table root = new Table();
 
-		LabeledButton button = new LabeledButton(Size.SMALL, "test.long");
-
-		root.add(button);
+		CustomLabel label = new CustomLabel("body1");
+		label.setUnderscoreColor(SkinColor.SECONDARY.getColor(SkinColor.ColorLevel._500));
+		label.setText("test");
+		root.add(label);
+		root.row().row();
+		CustomLabel label2 = new CustomLabel("h1");
+		label2.setUnderscoreColor(SkinColor.SECONDARY.getColor(SkinColor.ColorLevel._500));
+		label2.setText("test");
+		root.add(label2);
 
 		this.add(root);
 	}
--- core/src/main/java/pl/agh/edu/actor/utils/CustomLabel.java
+++ core/src/main/java/pl/agh/edu/actor/utils/CustomLabel.java
@@ -1,16 +1,42 @@
 package pl.agh.edu.actor.utils;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 
 import pl.agh.edu.actor.GameSkin;
 
-public abstract class CustomLabel extends Label {
+public class CustomLabel extends Label {
+	private Color underscoreColor = SkinSpecialColor.TRANSPARENT.getColor();
+
 	public CustomLabel(String font, String backgroundPatch) {
+		this(font);
+		getStyle().background = new NinePatchDrawable(GameSkin.getInstance().getPatch(backgroundPatch));
+	}
+
+	public CustomLabel(String font) {
 		super("", GameSkin.getInstance());
 		LabelStyle labelStyle = new LabelStyle();
 		labelStyle.font = GameSkin.getInstance().getFont(font);
-		labelStyle.background = new NinePatchDrawable(GameSkin.getInstance().getPatch(backgroundPatch));
 		this.setStyle(labelStyle);
+	}
+
+	public void setUnderscoreColor(Color color) {
+		this.underscoreColor = color;
+	}
+
+	@Override
+	public void draw(Batch batch, float parentAlpha) {
+		super.draw(batch, parentAlpha);
+
+		NinePatchDrawable underscoreDrawable = new NinePatchDrawable(GameSkin.getInstance().getPatch("underscore"));
+		underscoreDrawable = underscoreDrawable.tint(underscoreColor);
+
+		NinePatch underscorePatch = underscoreDrawable.getPatch();
+		underscorePatch.scale(1F, (float) Math.sqrt(getHeight() / 1000) + 0.05f);
+		underscorePatch.draw(batch, getX(), getY(), getWidth(), 5);
+
 	}
 }
