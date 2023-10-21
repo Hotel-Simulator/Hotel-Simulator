a7fdff6c839607761484430007241b1cab83ea4d
jagodek <godek.main@gmail.com>
2023-10-21 11:02:31 +0200

    HS-224 | spotless apply

--- core/src/main/java/pl/agh/edu/actor/utils/SkinColor.java
+++ core/src/main/java/pl/agh/edu/actor/utils/SkinColor.java
@@ -1,46 +1,48 @@
 package pl.agh.edu.actor.utils;
 
 import com.badlogic.gdx.graphics.Color;
+
 import pl.agh.edu.actor.GameSkin;
 
-    class SkinColor{
-        private Color color;
+class SkinColor {
+	private Color color;
 
-        SkinColor(ColorCategory colorCategory, ColorLevel colorLevel){
-            if (colorCategory == ColorCategory.TRANSPARENT || colorCategory == ColorCategory.SHADOW)
-                color = GameSkin.getInstance().getColor(colorCategory.value);
-            else
-                color = GameSkin.getInstance().getColor(colorCategory.value + "-" + colorLevel.value);
-        }
+	SkinColor(ColorCategory colorCategory, ColorLevel colorLevel) {
+		if (colorCategory == ColorCategory.TRANSPARENT || colorCategory == ColorCategory.SHADOW)
+			color = GameSkin.getInstance().getColor(colorCategory.value);
+		else
+			color = GameSkin.getInstance().getColor(colorCategory.value + colorLevel.value);
+	}
 
-        enum ColorCategory{
-            PRIMARY("primary"),
-            SECONDARY("secondary"),
-            ALERT("alert"),
-            SUCCESS("success"),
-            GRAY("gray"),
-            WARNING("warning"),
-            TRANSPARENT("transparent"),
-            SHADOW("shadow");
-            String value;
-            ColorCategory(String s) {
-                value = s;
-            }
-        }
+	enum ColorCategory {
+		PRIMARY("primary"),
+		SECONDARY("secondary"),
+		ALERT("alert"),
+		SUCCESS("success"),
+		GRAY("gray"),
+		WARNING("warning"),
+		TRANSPARENT("transparent"),
+		SHADOW("shadow");
 
-        enum ColorLevel{
-            _000("000"),
-            _100("100"),
-            _300("300"),
-            _500("500"),
-            _700("700"),
-            _900("900");
-            public final String value;
-            ColorLevel(String s) {
-                value = s;
-            }
-        }
-    }
+		String value;
 
+		ColorCategory(String s) {
+			value = s;
+		}
+	}
 
+	enum ColorLevel {
+		_000("-000"),
+		_100("-100"),
+		_300("-300"),
+		_500("-500"),
+		_700("-700"),
+		_900("-900");
 
+		public final String value;
+
+		ColorLevel(String s) {
+			value = s;
+		}
+	}
+}
