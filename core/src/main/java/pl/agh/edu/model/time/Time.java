741a37aca44fbbf102be5f4ddcf1cc0b9aec6be1
Jakub Spólnik <85324038+lawos98@users.noreply.github.com>
2023-10-21 09:52:52 +0200

    HS-220 | Add animations (#66)

--- core/src/main/java/pl/agh/edu/model/time/Time.java
+++ core/src/main/java/pl/agh/edu/model/time/Time.java
@@ -2,6 +2,8 @@ package pl.agh.edu.model.time;
 
 import java.time.LocalDateTime;
 import java.time.MonthDay;
+import java.util.ArrayList;
+import java.util.List;
 
 import pl.agh.edu.enums.PartOfDay;
 import pl.agh.edu.time_command.TimeCommandExecutor;
@@ -19,6 +21,9 @@ public class Time {
 	private boolean isRunning = false;
 	private float remaining = interval;
 	private final TimeCommandExecutor timeCommandExecutor = TimeCommandExecutor.getInstance();
+
+	private final List<Runnable> timeStopChangeHandlers = new ArrayList<>();
+	private final List<Runnable> timeStartChangeHandlers = new ArrayList<>();
 
 	private Time() {}
 
@@ -83,12 +88,18 @@ public class Time {
 		acceleration = Math.max(acceleration / 2, minAcceleration);
 	}
 
+	public int getAcceleration() {
+		return acceleration;
+	}
+
 	public void start() {
 		isRunning = true;
+		timeStartChangeHandlers.forEach(Runnable::run);
 	}
 
 	public void stop() {
 		isRunning = false;
+		timeStopChangeHandlers.forEach(Runnable::run);
 	}
 
 	public void toggle() {
@@ -138,4 +149,11 @@ public class Time {
 		return MonthDay.of(months, days);
 	}
 
+	public void addTimeStopChangeHandler(Runnable handler) {
+		timeStopChangeHandlers.add(handler);
+	}
+
+	public void addTimeStartChangeHandler(Runnable handler) {
+		timeStartChangeHandlers.add(handler);
+	}
 }
