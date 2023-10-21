4dc415407ecf384aea61c77d730a3cadcd7ce029
jagodek <godek.main@gmail.com>
2023-10-21 11:16:32 +0200

    HS-225 | LanguageLabel, CustomLabel modified

--- core/src/main/java/pl/agh/edu/actor/utils/CustomLabel.java
+++ core/src/main/java/pl/agh/edu/actor/utils/CustomLabel.java
@@ -1,16 +1,24 @@
 package pl.agh.edu.actor.utils;
 
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 
 import pl.agh.edu.actor.GameSkin;
 
-public abstract class CustomLabel extends Label {
-	public CustomLabel(String font, String backgroundPatch) {
+public class CustomLabel extends Label {
+	public CustomLabel(BitmapFont font, String backgroundPatch) {
 		super("", GameSkin.getInstance());
 		LabelStyle labelStyle = new LabelStyle();
-		labelStyle.font = GameSkin.getInstance().getFont(font);
+		labelStyle.font = font;
 		labelStyle.background = new NinePatchDrawable(GameSkin.getInstance().getPatch(backgroundPatch));
+		this.setStyle(labelStyle);
+	}
+
+	public CustomLabel(BitmapFont font) {
+		super("", GameSkin.getInstance());
+		LabelStyle labelStyle = new LabelStyle();
+		labelStyle.font = font;
 		this.setStyle(labelStyle);
 	}
 }
---
+++ core/src/main/java/pl/agh/edu/actor/utils/LanguageLabel.java
@@ -0,0 +1,34 @@
+package pl.agh.edu.actor.utils;
+
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
+
+import pl.agh.edu.actor.GameSkin;
+import pl.agh.edu.language.LanguageChangeListener;
+import pl.agh.edu.language.LanguageManager;
+
+public class LanguageLabel extends CustomLabel implements LanguageChangeListener {
+	private final String languagePath;
+
+	public LanguageLabel(String languagePath, BitmapFont font) {
+		super(font);
+		this.languagePath = languagePath;
+		LanguageManager.addListener(this);
+		onLanguageChange();
+	}
+
+	@Override
+	public void onLanguageChange() {
+		this.setText(LanguageManager.get(languagePath));
+	}
+
+	public void setUnderscore() {
+		LabelStyle labelStyle = new LabelStyle(getStyle());
+		NinePatchDrawable underscore = new NinePatchDrawable(GameSkin.getInstance().getPatch("underscore"));
+
+		// underscore = underscore.tint(SkinColor(SECONDARY, _500));
+
+		labelStyle.background = underscore;
+		setStyle(labelStyle);
+	}
+}
