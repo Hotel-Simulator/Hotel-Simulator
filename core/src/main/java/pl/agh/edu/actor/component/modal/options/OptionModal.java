579b4ce8195eaf2570e907380e2ab1d9f3511750
Jakub Spólnik <jakubspolnik@student.agh.edu.pl>
2023-10-22 17:16:23 +0200

    HS-229 | Improve options frame

--- core/src/main/java/pl/agh/edu/actor/component/modal/options/OptionModal.java
+++ core/src/main/java/pl/agh/edu/actor/component/modal/options/OptionModal.java
@@ -1,11 +1,14 @@
 package pl.agh.edu.actor.component.modal.options;
 
+import static pl.agh.edu.actor.utils.FontType.H2;
+
 import java.util.function.Function;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
 
 import pl.agh.edu.actor.component.button.LabeledButton;
 import pl.agh.edu.actor.component.selectMenu.SelectMenu;
@@ -15,6 +18,7 @@ import pl.agh.edu.actor.component.selectMenu.SelectMenuLanguage;
 import pl.agh.edu.actor.component.selectMenu.SelectMenuResolutionItem;
 import pl.agh.edu.actor.component.slider.PercentSliderComponent;
 import pl.agh.edu.actor.component.slider.SliderComponent;
+import pl.agh.edu.actor.utils.LanguageLabel;
 import pl.agh.edu.actor.utils.resolution.Size;
 import pl.agh.edu.actor.utils.wrapper.WrapperTable;
 import pl.agh.edu.audio.SoundAudio;
@@ -30,20 +34,23 @@ public class OptionModal extends WrapperTable {
 	public OptionModal(Runnable closeHandler) {
 		this.setBackground("modal-glass-background");
 
-		innerTable.add(selectResolutionMenu).grow().row();
-		innerTable.add(selectFullScreenMenu).grow().row();
+		LanguageLabel titleLabel = new LanguageLabel("optionsFrame.label.title", H2.getName());
+		titleLabel.setAlignment(Align.center, Align.center);
+		innerTable.add(titleLabel).growX().center().row();
+
+		innerTable.add(selectResolutionMenu).grow().pad(OptionFrameStyle.getInnerPadding()).row();
+		innerTable.add(selectFullScreenMenu).grow().pad(OptionFrameStyle.getInnerPadding()).row();
 		SliderComponent musicVolumeSlider = createSliderComponentForMusicVolume();
-		innerTable.add(musicVolumeSlider).grow().row();
+		innerTable.add(musicVolumeSlider).grow().pad(OptionFrameStyle.getInnerPadding()).row();
 		SliderComponent soundVolumeSlider = createSliderComponentForSoundVolume();
-		innerTable.add(soundVolumeSlider).grow().row();
-		innerTable.add(selectLanguageMenu).grow();
-		innerTable.row().padTop(OptionFrameStyle.getPadding() / 2);
+		innerTable.add(soundVolumeSlider).grow().pad(OptionFrameStyle.getInnerPadding()).row();
+		innerTable.add(selectLanguageMenu).grow().pad(OptionFrameStyle.getInnerPadding()).row();
 
 		Table ButtonTable = new Table();
 		LabeledButton backButton = new LabeledButton(Size.LARGE, "optionsFrame.label.back");
-		ButtonTable.add(backButton).grow().uniform();
+		ButtonTable.add(backButton).pad(OptionFrameStyle.getInnerPadding()).grow().uniform();
 		LabeledButton saveButton = new LabeledButton(Size.LARGE, "optionsFrame.label.save");
-		ButtonTable.add(saveButton).grow().uniform();
+		ButtonTable.add(saveButton).pad(OptionFrameStyle.getInnerPadding()).grow().uniform();
 
 		backButton.addListener(new ClickListener() {
 			@Override
@@ -61,7 +68,7 @@ public class OptionModal extends WrapperTable {
 			}
 		});
 
-		innerTable.add(ButtonTable).grow();
+		innerTable.add(ButtonTable).growX();
 		innerTable.pad(OptionFrameStyle.getPadding());
 
 		setStartingValue();
@@ -164,17 +171,17 @@ public class OptionModal extends WrapperTable {
 	private static class OptionFrameStyle {
 		public static float getFrameHeight() {
 			return switch (GraphicConfig.getResolution().SIZE) {
-				case SMALL -> 400f + 2 * getPadding();
-				case MEDIUM -> 500f + 2 * getPadding();
-				case LARGE -> 600f + 2 * getPadding();
+				case SMALL -> 450f + 2 * getPadding();
+				case MEDIUM -> 600f + 2 * getPadding();
+				case LARGE -> 700f + 2 * getPadding();
 			};
 		}
 
 		public static float getFrameWidth() {
 			return switch (GraphicConfig.getResolution().SIZE) {
-				case SMALL -> 650f + 2 * getPadding();
-				case MEDIUM -> 750f + 2 * getPadding();
-				case LARGE -> 900f + 2 * getPadding();
+				case SMALL -> 700f + 2 * getPadding();
+				case MEDIUM -> 800f + 2 * getPadding();
+				case LARGE -> 1000f + 2 * getPadding();
 			};
 		}
 
@@ -184,6 +191,10 @@ public class OptionModal extends WrapperTable {
 				case MEDIUM -> 40f;
 				case LARGE -> 50f;
 			};
+		}
+
+		public static float getInnerPadding() {
+			return 10f;
 		}
 	}
 }
