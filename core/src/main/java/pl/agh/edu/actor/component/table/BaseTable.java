2825e409af518b32ecba656b1181bbf57076054d
jagodek <godek.main@gmail.com>
2023-10-25 18:28:40 +0200

    HS-140 | messs after merge with main

--- core/src/main/java/pl/agh/edu/actor/component/table/BaseTable.java
+++ core/src/main/java/pl/agh/edu/actor/component/table/BaseTable.java
@@ -2,22 +2,75 @@ package pl.agh.edu.actor.component.table;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Cell;
+import com.badlogic.gdx.scenes.scene2d.ui.Container;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.compression.lzma.Base;
+import pl.agh.edu.actor.GameSkin;
 import pl.agh.edu.actor.utils.FontType;
+import pl.agh.edu.actor.utils.LanguageLabel;
 import pl.agh.edu.actor.utils.wrapper.WrapperTable;
 import pl.agh.edu.config.GraphicConfig;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static pl.agh.edu.actor.component.table.BaseTable.BaseTableStyle.getCellPadding;
+import static pl.agh.edu.actor.component.table.BaseTable.BaseTableStyle.getRowSpacing;
+import static pl.agh.edu.actor.component.table.CreditTable.CreditTableStyle.getSeparatorWidth;
+import static pl.agh.edu.actor.component.table.CreditTable.CreditTableStyle.getTableFont;
+
 public abstract class BaseTable extends WrapperTable {
+	protected int noColumns;
+	protected Table contentRows = new Table();
+	protected ScrollPane scrollPane = new ScrollPane(contentRows, GameSkin.getInstance(), "transparent");
 
 	public BaseTable() {
 		super();
+
 	}
 
-	public abstract class BaseRow extends WrapperTable {
-		BaseRow() {
+	protected void addRow(BaseRow row, Table targetTable){
+		row.align(Align.topLeft);
+		targetTable.add(row).spaceBottom(getRowSpacing()).growX().align(Align.topLeft);
+		targetTable.row();
+	}
+	protected void deleteRow(BaseRow row) {
+		Cell<BaseRow> cell = contentRows.getCell(row);
+		cell.space(0);
+		contentRows.getCell(row).clearActor();
+	}
+	protected abstract class BaseRow extends WrapperTable {
+
+
+		BaseRow(List<String> columnNames) {
+			super();
+			noColumns = columnNames.size();
+			insertActorsToRow(columnNames.stream().map(s -> new LanguageLabel(s, getTableFont().getName())).collect(Collectors.toList()));
+			this.setBackground("table-header-background");
+		}
+		BaseRow(){
 			super();
 		}
 
-		public abstract void insertActorsToRow(Actor... actors);
+		public void insertActorsToRow(List<Actor> actors) {
+			IntStream.range(0, noColumns).forEach(i -> {
+				Actor actor = actors.get(i);
+				Container<Actor> container = new Container<>(actor);
+				this.innerTable.add(container).growX().uniform().center().padLeft(getCellPadding()).padRight(getCellPadding());
+				if (i != noColumns - 1)
+					this.innerTable.add(new Image(GameSkin.getInstance().getPatch("table-separator-line"))).width(getSeparatorWidth()).growY().center();
+			});
+		}
+
+
+
+
 	}
 
 	public abstract static class BaseTableStyle {
--- core/src/main/java/pl/agh/edu/actor/component/table/CreditTable.java
+++ core/src/main/java/pl/agh/edu/actor/component/table/CreditTable.java
@@ -3,15 +3,17 @@ package pl.agh.edu.actor.component.table;
 import static pl.agh.edu.actor.component.table.CreditTable.CreditTableStyle.*;
 
 import java.util.Arrays;
-import java.util.stream.IntStream;
+import java.util.List;
 
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.ui.Container;
+
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.utils.Align;
 
+import com.badlogic.gdx.utils.Array;
 import pl.agh.edu.actor.GameSkin;
 import pl.agh.edu.actor.utils.FontType;
 import pl.agh.edu.actor.utils.LanguageLabel;
@@ -21,32 +23,65 @@ import pl.agh.edu.management.bank.BankAccountHandler;
 import pl.agh.edu.model.bank.Credit;
 
 public class CreditTable extends BaseTable {
-	private final int noColumns = 3;
-	private final BankAccountHandler bankAccountHandler;
 
+	private final BankAccountHandler bankAccountHandler;
+	public static int counter = 0;
 	public CreditTable(BankAccountHandler bankAccountHandler) {
 		super();
 		this.bankAccountHandler = bankAccountHandler;
-		CreditBaseRow creditHeaderRow = new CreditBaseRow("creditTable.column.date", "creditTable.column.monthly", "creditTable.column.payall");
-		creditHeaderRow.align(Align.bottomLeft);
-		innerTable.add(creditHeaderRow).space(getRowSpacing()).growX().align(Align.bottomLeft);
-		innerTable.row();
+		CreditBaseRow creditHeaderRow = new CreditBaseRow();
+//		ScrollPane sp = new ScrollPane(creditHeaderRow, GameSkin.getInstance(), "transparent");
+//		addRow(creditHeaderRow, innerTable);
+		creditHeaderRow.align(Align.topLeft);
+		
+		innerTable.add(creditHeaderRow).height(getRowHeight()+getRowSpacing()).spaceBottom(getRowSpacing()).growX().align(Align.topLeft).row();
+		innerTable.getCells().get(0).padRight(scrollPane.getScrollBarWidth());
+		System.out.println("scrollPane.getScrollBarWidth() = " + scrollPane.getScrollBarWidth());
+		counter++;
+		System.out.println("counter = " + counter);
+
+//		innerTable.add(sp).height(getRowHeight()+2*getRowSpacing()).fill().space(getRowSpacing()).row();
+//		sp.validate();
 		for (Credit credit : bankAccountHandler.getCurrentCredits().keySet()) {
 			BaseRow row = new CreditBaseRow(credit);
-			row.align(Align.bottomLeft);
-			innerTable.add(row).space(getRowSpacing()).growX().align(Align.bottomLeft);
-			innerTable.row();
+			addRow(row, contentRows);
 		}
+//		Drawable knobDrawable =GameSkin.getInstance().getDrawable("scroll-pane-knob");
+//		Image knobImage = new Image(knobDrawable);
+//		innerTable.add(knobImage);
+//		innerTable.row();
+
+		contentRows.align(Align.topLeft);
+
+		scrollPane.setFadeScrollBars(false);
+//		scrollPane.setScrollbarsVisible(true);
+
+//		scrollPane.setScrollBarPositions(false,true);
+//		System.out.println("scrollPane.getScrollWidth() = " + scrollPane.getScrollWidth());
+		scrollPane.setWidth(scrollPane.getWidth()  + scrollPane.getScrollWidth());
+		innerTable.add(scrollPane).growX().align(Align.topLeft).colspan(2);
+		scrollPane.setForceScroll(false,true);
+
+//		System.out.println( "scrollbarwidth "+scrollPane.getScrollBarWidth());
+//		System.out.println( "scrollwidth "+scrollPane.getScrollWidth());
+//		scrollPane.debugAll();
+//		innerTable.debugAll();
 
 	}
+
+	public void layout() {
+		super.layout();
+		System.out.println("scrollPane.getScrollBarWidth() = " + scrollPane.getScrollBarWidth());
+		innerTable.setWidth(getWidth()-scrollPane.getScrollBarWidth());
+	}
+
+
 
 	public class CreditBaseRow extends BaseRow {
 		private final Skin skin = GameSkin.getInstance();
 
-		public CreditBaseRow(String... columnNames) {
-			super();
-			insertActorsToRow(Arrays.stream(columnNames).map(s -> new LanguageLabel(s, getTableFont().getName())).toArray(Actor[]::new));
-			this.setBackground("table-header-background");
+		public CreditBaseRow() {
+			super(List.of("creditTable.column.date", "creditTable.column.monthly", "creditTable.column.payall"));
 		}
 
 		public CreditBaseRow(Credit credit) {
@@ -59,26 +94,14 @@ public class CreditTable extends BaseTable {
 				deleteRow(this);
 			});
 			payAllButton.setVarArgs(bankAccountHandler.getValueLeftToPay(credit).stripTrailingZeros());
-			insertActorsToRow(date, monthly, payAllButton);
+			insertActorsToRow(List.of(date, monthly, payAllButton));
 			this.setBackground("table-row-background");
 		}
 
-		public void insertActorsToRow(Actor... actors) {
-			IntStream.range(0, noColumns).forEach(i -> {
-				Actor actor = actors[i];
-				Container<Actor> container = new Container<>(actor);
-				container.pad(1f);
-				this.innerTable.add(container).growX().uniform().center().padLeft(getCellPadding()).padRight(getCellPadding());
-				if (i != noColumns - 1)
-					this.innerTable.add(new Image(GameSkin.getInstance().getPatch("table-separator-line"))).width(getSeparatorWidth()).growY().center();
-			});
-		}
+
 	}
 
-	private void deleteRow(CreditBaseRow row) {
-		innerTable.getCell(row).clearActor();
-		row.remove();
-	}
+
 
 	public static class CreditTableStyle extends BaseTableStyle {
 		public static float getRowHeight() {
