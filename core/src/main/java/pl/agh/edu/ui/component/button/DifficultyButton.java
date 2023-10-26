d11f22efe8c259b348f17a3df530b42044b50ed2
Bartosz Dudek <dudekb@student.agh.edu.pl>
2023-10-26 20:19:49 +0200

    styles

--- core/src/main/java/pl/agh/edu/ui/component/button/DifficultyButton.java
+++ core/src/main/java/pl/agh/edu/ui/component/button/DifficultyButton.java
@@ -1,21 +1,52 @@
 package pl.agh.edu.ui.component.button;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import pl.agh.edu.config.GraphicConfig;
 import pl.agh.edu.engine.hotel.dificulty.DifficultyLevel;
+import pl.agh.edu.ui.language.LanguageChangeListener;
+import pl.agh.edu.ui.language.LanguageManager;
 
-public class DifficultyButton extends TextButton {
+public class DifficultyButton extends TextButton implements LanguageChangeListener {
     public final DifficultyLevel difficulty;
+    public final Skin skin;
 
-    public DifficultyButton(String text, TextButtonStyle style, DifficultyLevel difficulty) {
-        super(text, style);
+    public DifficultyButton(DifficultyLevel difficulty, Skin skin, Boolean isChecked) {
+        super("", skin);
         this.difficulty = difficulty;
+        this.skin = skin;
 
-//        if (scenariosSettings.getDifficultyButton().isPresent() && scenariosSettings.getDifficultyButton().get().difficulty == difficulty) {
-//            setChecked(true);
-//        }
+        setButtonText();
+        setChecked(isChecked);
+    }
+
+    public DifficultyButton(DifficultyLevel difficulty, Skin skin) {
+        super("", skin);
+        this.difficulty = difficulty;
+        this.skin = skin;
+
+        setButtonText();
     }
 
     public DifficultyLevel getDifficulty() {
         return difficulty;
+    }
+
+    public void setButtonText(){
+        String path = "difficulty." + difficulty.toString();
+        setText(LanguageManager.get(path));
+    }
+
+    public TextButtonStyle getStyle(){
+        return switch(GraphicConfig.getResolution().SIZE){
+            case SMALL -> skin.get("difficulty-button-subtitle1", TextButtonStyle.class);
+            case MEDIUM -> skin.get("difficulty-button-h4", TextButtonStyle.class);
+            case LARGE -> skin.get("difficulty-button-h3", TextButtonStyle.class);
+        };
+    }
+
+    @Override
+    public void onLanguageChange() {
+        setButtonText();
     }
 }
