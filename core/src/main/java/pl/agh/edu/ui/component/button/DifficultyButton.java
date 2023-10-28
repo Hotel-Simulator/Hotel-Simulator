14d104644516dc44bdfa6f705b539094b25191fd
Bartosz Dudek <dudekb@student.agh.edu.pl>
2023-10-28 10:50:02 +0200

    difficulty button ready

--- core/src/main/java/pl/agh/edu/ui/component/button/DifficultyButton.java
+++ core/src/main/java/pl/agh/edu/ui/component/button/DifficultyButton.java
@@ -2,51 +2,53 @@ package pl.agh.edu.ui.component.button;
 
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+
 import pl.agh.edu.config.GraphicConfig;
 import pl.agh.edu.engine.hotel.dificulty.DifficultyLevel;
 import pl.agh.edu.ui.language.LanguageChangeListener;
 import pl.agh.edu.ui.language.LanguageManager;
+import pl.agh.edu.ui.resolution.ResolutionChangeListener;
+import pl.agh.edu.ui.resolution.ResolutionManager;
 
-public class DifficultyButton extends TextButton implements LanguageChangeListener {
-    public final DifficultyLevel difficulty;
-    public final Skin skin;
+public class DifficultyButton extends TextButton implements LanguageChangeListener, ResolutionChangeListener {
+	public final DifficultyLevel difficulty;
+	public final Skin skin;
 
-    public DifficultyButton(DifficultyLevel difficulty, Skin skin, Boolean isChecked) {
-        super("", skin);
-        this.difficulty = difficulty;
-        this.skin = skin;
+	public DifficultyButton(DifficultyLevel difficulty, Skin skin) {
+		super("", skin);
+		this.difficulty = difficulty;
+		this.skin = skin;
+		this.setStyle(getStyle());
 
-        setButtonText();
-        setChecked(isChecked);
-    }
+		setButtonText();
+		listenForChanges();
+	}
 
-    public DifficultyButton(DifficultyLevel difficulty, Skin skin) {
-        super("", skin);
-        this.difficulty = difficulty;
-        this.skin = skin;
+	private void listenForChanges() {
+		ResolutionManager.addListener(this);
+		LanguageManager.addListener(this);
+	}
 
-        setButtonText();
-    }
+	public void setButtonText() {
+		String path = "difficulty." + difficulty.toString().toLowerCase();
+		setText(LanguageManager.get(path));
+	}
 
-    public DifficultyLevel getDifficulty() {
-        return difficulty;
-    }
+	public TextButtonStyle getStyle() {
+		return switch (GraphicConfig.getResolution().SIZE) {
+			case SMALL -> skin.get("difficulty-button-small", TextButtonStyle.class);
+			case MEDIUM -> skin.get("difficulty-button-medium", TextButtonStyle.class);
+			case LARGE -> skin.get("difficulty-button-large", TextButtonStyle.class);
+		};
+	}
 
-    public void setButtonText(){
-        String path = "difficulty." + difficulty.toString();
-        setText(LanguageManager.get(path));
-    }
+	@Override
+	public void onLanguageChange() {
+		setButtonText();
+	}
 
-    public TextButtonStyle getStyle(){
-        return switch(GraphicConfig.getResolution().SIZE){
-            case SMALL -> skin.get("difficulty-button-subtitle1", TextButtonStyle.class);
-            case MEDIUM -> skin.get("difficulty-button-h4", TextButtonStyle.class);
-            case LARGE -> skin.get("difficulty-button-h3", TextButtonStyle.class);
-        };
-    }
-
-    @Override
-    public void onLanguageChange() {
-        setButtonText();
-    }
+	@Override
+	public void onResolutionChange() {
+		setStyle(getStyle());
+	}
 }
