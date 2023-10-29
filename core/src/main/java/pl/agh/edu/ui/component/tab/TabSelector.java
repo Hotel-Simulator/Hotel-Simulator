abe18ccf36b208bf7a2125f9fec974703279f813
jagodek <godek.main@gmail.com>
2023-10-29 17:50:05 +0100

    HS-200 | fixes

--- core/src/main/java/pl/agh/edu/ui/component/tab/TabSelector.java
+++ core/src/main/java/pl/agh/edu/ui/component/tab/TabSelector.java
@@ -1,44 +1,58 @@
 package pl.agh.edu.ui.component.tab;
 
+import static pl.agh.edu.ui.component.tab.TabSelector.TabSelectorStyle.getFont;
+import static pl.agh.edu.ui.utils.SkinColor.GRAY;
+
 import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
+
+import pl.agh.edu.config.GraphicConfig;
 import pl.agh.edu.ui.GameSkin;
-import pl.agh.edu.ui.utils.FontType;
 import pl.agh.edu.ui.component.label.LanguageLabel;
+import pl.agh.edu.ui.utils.FontType;
 import pl.agh.edu.ui.utils.SkinColor.ColorLevel;
 import pl.agh.edu.ui.utils.wrapper.WrapperTable;
 
-import static pl.agh.edu.ui.utils.SkinColor.GRAY;
-
 public class TabSelector extends WrapperTable {
-  Skin skin = GameSkin.getInstance();
-  boolean left = true;
-  public TabSelector(String laguagePathLeft, String languagePathRight, Runnable actionLeft, Runnable actionRight) {
-    super();
-    LanguageLabel rightLinkLabel = new LanguageLabel(languagePathRight, FontType.BODY_1.getWhiteVariantName());
-    LanguageLabel leftLinkLabel = new LanguageLabel(laguagePathLeft, FontType.BODY_1.getWhiteVariantName());
+	Skin skin = GameSkin.getInstance();
+	boolean left = true;
 
-    Runnable newActionLeft = () -> {
-      actionLeft.run();
-      leftLinkLabel.setDisabled(true);
-      rightLinkLabel.setDisabled(false);
-      left = true;
-    };
-    Runnable newActionRight = () -> {
-      actionRight.run();
-      rightLinkLabel.setDisabled(true);
-      leftLinkLabel.setDisabled(false);
-      left = false;
-    };
-    leftLinkLabel.makeItLink(newActionLeft);
-    rightLinkLabel.makeItLink(newActionRight);
-    innerTable.add(leftLinkLabel).pad(10);
-    NinePatch separator = skin.getPatch("tabs-separator-line");
-    separator.setColor(GRAY.getColor(ColorLevel._500));
-    innerTable.add(new Image(separator)).width(2).pad(10).padTop(20).padBottom(20).padLeft(0).padRight(0);
-    innerTable.add(rightLinkLabel).pad(10);
-    newActionLeft.run();
-  }
+	public TabSelector(String laguagePathLeft, String languagePathRight, Runnable actionLeft, Runnable actionRight) {
+		super();
+		LanguageLabel rightLinkLabel = new LanguageLabel(languagePathRight, getFont());
+		LanguageLabel leftLinkLabel = new LanguageLabel(laguagePathLeft, getFont());
+
+		Runnable newActionLeft = () -> {
+			actionLeft.run();
+			leftLinkLabel.setDisabled(true);
+			rightLinkLabel.setDisabled(false);
+			left = true;
+		};
+		Runnable newActionRight = () -> {
+			actionRight.run();
+			rightLinkLabel.setDisabled(true);
+			leftLinkLabel.setDisabled(false);
+			left = false;
+		};
+		leftLinkLabel.makeItLink(newActionLeft);
+		rightLinkLabel.makeItLink(newActionRight);
+		innerTable.add(leftLinkLabel).pad(10);
+		NinePatch separator = skin.getPatch("tabs-separator-line");
+		separator.setColor(GRAY.getColor(ColorLevel._500));
+		innerTable.add(new Image(separator)).width(2).pad(10).padTop(20).padBottom(20).padLeft(10).padRight(10);
+		innerTable.add(rightLinkLabel).pad(10);
+		newActionLeft.run();
+	}
+
+	static class TabSelectorStyle {
+		static String getFont() {
+			return switch (GraphicConfig.getResolution().SIZE) {
+				case SMALL -> FontType.BODY_2.getWhiteVariantName();
+				case MEDIUM -> FontType.BODY_1.getWhiteVariantName();
+				case LARGE -> FontType.H4.getWhiteVariantName();
+			};
+		}
+	}
+
 }
--- core/src/main/java/pl/agh/edu/ui/frame/TestFrame.java
+++ core/src/main/java/pl/agh/edu/ui/frame/TestFrame.java
@@ -1,58 +1,11 @@
 package pl.agh.edu.ui.frame;
 
-import java.math.BigDecimal;
-
-import com.badlogic.gdx.utils.Align;
-
-import pl.agh.edu.engine.bank.BankAccount;
-import pl.agh.edu.engine.bank.BankAccountHandler;
 import pl.agh.edu.ui.component.tab.TabSelector;
-import pl.agh.edu.ui.component.table.CreditTable;
 
 public class TestFrame extends BaseFrame {
 	public TestFrame(String languagePath) {
 		super(languagePath);
-
-
-		// CUSTOM AND LANGUAGE LABELS
-		//
-		// CustomLabel label = new CustomLabel("body1");
-		// label.setText("test");
-		// mainTable.add(label);
-		// mainTable.row().row();
-		//
-		// CustomLabel label2 = new CustomLabel(H1.getWhiteVariantName());
-		// label2.setUnderscoreColor(SkinColor.SECONDARY.getColor(SkinColor.ColorLevel._500));
-		// label2.setText("test");
-		// label2.makeItLink(() -> System.out.println("h1"));
-		// mainTable.add(label2);
-		//
-		// LanguageLabel languageLabel1 = new LanguageLabel("test.test", BODY_1.getWhiteVariantName());
-		// mainTable.add(languageLabel1).row();
-		// languageLabel1.makeItLink(() -> System.out.println("test"));
-		// languageLabel1.setDisabled(true);
-
-
-//		CREDIT TABLE
-//
-//		BankAccount bankAccount = new BankAccount(BigDecimal.ONE, BigDecimal.TEN, BigDecimal.TEN);
-//		BankAccountHandler bankAccountHandler = new BankAccountHandler(bankAccount);
-//		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
-//		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
-//		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
-//		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
-//		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
-//		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
-//		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
-//		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
-//		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
-//		CreditTable creditTable = new CreditTable(bankAccountHandler);
-//
-//		creditTable.align(Align.left);
-//		mainTable.add(creditTable).grow().align(Align.topLeft);
-
 		mainTable.add(new TabSelector("test.test", "test.test", () -> System.out.println("left"), () -> System.out.println("right"))).row();
-
 
 	}
 }
