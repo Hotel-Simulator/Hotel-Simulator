323790be3e813f30bc973848624c9269fcdd30ba
Jakub Spólnik <jakubspolnik@student.agh.edu.pl>
2023-10-29 15:47:39 +0100

    HS-138 | Add example of use

--- core/src/main/java/pl/agh/edu/engine/advertisement/AdvertisementDataManipulator.java
+++ core/src/main/java/pl/agh/edu/engine/advertisement/AdvertisementDataManipulator.java
@@ -5,13 +5,7 @@ import pl.agh.edu.utils.DataManipulator;
 import java.time.LocalDate;
 
 public class AdvertisementDataManipulator extends DataManipulator<AdvertisementCampaign> {
-    public AdvertisementDataManipulator() {
-        addFilter(new AdvertisementTypeFilter());
-        addFilter(new AdvertisementDateBefore());
-        addSorter(new AdvertisementStartDateSorter());
-    }
-
-
+    private AdvertisementDataManipulator() {}
 
     private static class AdvertisementTypeFilter implements Filter<AdvertisementCampaign> {
         @Override
@@ -19,15 +13,81 @@ public class AdvertisementDataManipulator extends DataManipulator<AdvertisementC
             return advertisementCampaign.advertisementData().type() == AdvertisementType.NEWSPAPER_ADVERTISEMENT;
         }
     }
-    private class AdvertisementDateBefore implements Filter<AdvertisementCampaign> {
-
+    private static class AdvertisementStartDateAfterFilter implements Filter<AdvertisementCampaign> {
         private final LocalDate date;
-        public AdvertisementDateBefore(LocalDate date) {
+        public AdvertisementStartDateAfterFilter(LocalDate date) {
             this.date = date;
         }
         @Override
         public boolean filter(AdvertisementCampaign advertisementCampaign) {
-            return advertisementCampaign.startDate().isBefore(date);
+            return advertisementCampaign.startDate().isAfter(date);
+        }
+    }
+
+    private static class AdvertisementEndDateBeforeFilter implements Filter<AdvertisementCampaign> {
+        private final LocalDate date;
+        public AdvertisementEndDateBeforeFilter(LocalDate date) {
+            this.date = date;
+        }
+        @Override
+        public boolean filter(AdvertisementCampaign advertisementCampaign) {
+            return advertisementCampaign.endDate().isBefore(date);
+        }
+    }
+    private static class AdvertisementEndDateSorter implements Sorter<AdvertisementCampaign> {
+        @Override
+        public int compare(AdvertisementCampaign advertisementCampaign1, AdvertisementCampaign advertisementCampaign2) {
+            return advertisementCampaign1.endDate().compareTo(advertisementCampaign2.endDate());
+        }
+    }
+
+    private static class AdvertismetnStartDateSorter implements Sorter<AdvertisementCampaign> {
+        @Override
+        public int compare(AdvertisementCampaign advertisementCampaign1, AdvertisementCampaign advertisementCampaign2) {
+            return advertisementCampaign1.startDate().compareTo(advertisementCampaign2.startDate());
+        }
+    }
+
+    private static class AdvertisementTypeSorter implements Sorter<AdvertisementCampaign> {
+        @Override
+        public int compare(AdvertisementCampaign advertisementCampaign1, AdvertisementCampaign advertisementCampaign2) {
+            return advertisementCampaign1.advertisementData().type().compareTo(advertisementCampaign2.advertisementData().type());
+        }
+    }
+
+
+    public static class AdvertisementDataManipulatorBuilder {
+        private final AdvertisementDataManipulator manipulator;
+
+        public AdvertisementDataManipulatorBuilder() {
+            manipulator = new AdvertisementDataManipulator();
+        }
+        public AdvertisementDataManipulatorBuilder withStartDateAfterFilter(LocalDate date) {
+            manipulator.addFilter(new AdvertisementStartDateAfterFilter(date));
+            return this;
+        }
+
+        public AdvertisementDataManipulatorBuilder withEndDateBeforeFilter(LocalDate date) {
+            manipulator.addFilter(new AdvertisementEndDateBeforeFilter(date));
+            return this;
+        }
+        public AdvertisementDataManipulatorBuilder sortByEndDate() {
+            manipulator.addSorter(new AdvertisementEndDateSorter());
+            return this;
+        }
+
+        public AdvertisementDataManipulatorBuilder sortByStartDate() {
+            manipulator.addSorter(new AdvertismetnStartDateSorter());
+            return this;
+        }
+
+        public AdvertisementDataManipulatorBuilder sortByAdvertisementType() {
+            manipulator.addSorter(new AdvertisementTypeSorter());
+            return this;
+        }
+
+        public AdvertisementDataManipulator build() {
+            return manipulator;
         }
     }
 }
