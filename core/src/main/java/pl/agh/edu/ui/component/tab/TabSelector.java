f2feca2c1394080b3a988fca1cc9946629e84533
Michał Godek <mgodek@student.agh.edu.pl>
2023-10-29 19:01:42 +0100

    HS-200 | tabs  (#91)
    * HS-200 | it is what it is
    
    * checkpoint
    
    * HS-200 | Tabs
    
    * HS-200 | fixes
    
    * HS-200 | requested change
    
    * HS-200 | typo
    
    * HS-200 | skin update
    
    * HS-200 | another skin update?

---
+++ core/src/main/java/pl/agh/edu/ui/component/tab/TabSelector.java
@@ -0,0 +1,55 @@
+package pl.agh.edu.ui.component.tab;
+
+import static pl.agh.edu.ui.component.tab.TabSelector.TabSelectorStyle.getFont;
+import static pl.agh.edu.ui.utils.SkinColor.GRAY;
+
+import com.badlogic.gdx.graphics.g2d.NinePatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+import pl.agh.edu.config.GraphicConfig;
+import pl.agh.edu.ui.GameSkin;
+import pl.agh.edu.ui.component.label.LanguageLabel;
+import pl.agh.edu.ui.utils.FontType;
+import pl.agh.edu.ui.utils.SkinColor.ColorLevel;
+import pl.agh.edu.ui.utils.wrapper.WrapperTable;
+
+public class TabSelector extends WrapperTable {
+	Skin skin = GameSkin.getInstance();
+
+	public TabSelector(String languagePathLeft, String languagePathRight, Runnable actionLeft, Runnable actionRight) {
+		super();
+		LanguageLabel rightLinkLabel = new LanguageLabel(languagePathRight, getFont());
+		LanguageLabel leftLinkLabel = new LanguageLabel(languagePathLeft, getFont());
+
+		Runnable newActionLeft = () -> {
+			actionLeft.run();
+			leftLinkLabel.setDisabled(true);
+			rightLinkLabel.setDisabled(false);
+		};
+		Runnable newActionRight = () -> {
+			actionRight.run();
+			rightLinkLabel.setDisabled(true);
+			leftLinkLabel.setDisabled(false);
+		};
+		leftLinkLabel.makeItLink(newActionLeft);
+		rightLinkLabel.makeItLink(newActionRight);
+		innerTable.add(leftLinkLabel).pad(10);
+		NinePatch separator = skin.getPatch("tabs-separator-line");
+		separator.setColor(GRAY.getColor(ColorLevel._500));
+		innerTable.add(new Image(separator)).width(2).pad(10).padTop(20).padBottom(20).padLeft(10).padRight(10);
+		innerTable.add(rightLinkLabel).pad(10);
+		newActionLeft.run();
+	}
+
+	static class TabSelectorStyle {
+		static String getFont() {
+			return switch (GraphicConfig.getResolution().SIZE) {
+				case SMALL -> FontType.BODY_2.getWhiteVariantName();
+				case MEDIUM -> FontType.BODY_1.getWhiteVariantName();
+				case LARGE -> FontType.H4.getWhiteVariantName();
+			};
+		}
+	}
+
+}
