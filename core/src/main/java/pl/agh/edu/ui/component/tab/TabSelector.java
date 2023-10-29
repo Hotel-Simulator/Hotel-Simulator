61b865db6a73cde71b75ec4d1e15e9efce022ddb
jagodek <godek.main@gmail.com>
2023-10-29 17:30:32 +0100

    HS-200 | Tabs

--- core/src/main/java/pl/agh/edu/ui/component/tab/TabSelector.java
+++ core/src/main/java/pl/agh/edu/ui/component/tab/TabSelector.java
@@ -1,44 +1,44 @@
 package pl.agh.edu.ui.component.tab;
 
+import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 import pl.agh.edu.ui.GameSkin;
 import pl.agh.edu.ui.utils.FontType;
-import pl.agh.edu.ui.utils.LinkLabel;
+import pl.agh.edu.ui.component.label.LanguageLabel;
+import pl.agh.edu.ui.utils.SkinColor.ColorLevel;
 import pl.agh.edu.ui.utils.wrapper.WrapperTable;
 
+import static pl.agh.edu.ui.utils.SkinColor.GRAY;
+
 public class TabSelector extends WrapperTable {
-  private final String languagePathLeft;
-  private final String languagePathRight;
   Skin skin = GameSkin.getInstance();
   boolean left = true;
-
-
   public TabSelector(String laguagePathLeft, String languagePathRight, Runnable actionLeft, Runnable actionRight) {
     super();
-    this.languagePathLeft = laguagePathLeft;
-    this.languagePathRight = languagePathRight;
-    LinkLabel rightLinkLabel = new LinkLabel(languagePathRight, FontType.BODY_1.getWhiteVariantName(), actionLeft);
-    LinkLabel leftLinkLabel = new LinkLabel(laguagePathLeft, FontType.BODY_1.getWhiteVariantName(), actionRight);
+    LanguageLabel rightLinkLabel = new LanguageLabel(languagePathRight, FontType.BODY_1.getWhiteVariantName());
+    LanguageLabel leftLinkLabel = new LanguageLabel(laguagePathLeft, FontType.BODY_1.getWhiteVariantName());
+
     Runnable newActionLeft = () -> {
-      System.out.println("dupa");
       actionLeft.run();
       leftLinkLabel.setDisabled(true);
       rightLinkLabel.setDisabled(false);
       left = true;
     };
-    Runnable neActionRight = () -> {
-      System.out.println("dupa");
+    Runnable newActionRight = () -> {
       actionRight.run();
       rightLinkLabel.setDisabled(true);
       leftLinkLabel.setDisabled(false);
       left = false;
     };
-    leftLinkLabel.setLinkAction(newActionLeft);
-    rightLinkLabel.setLinkAction(neActionRight);
-
-    innerTable.add(new LinkLabel(laguagePathLeft, FontType.BODY_1.getWhiteVariantName(), actionLeft)).pad(10);
-    innerTable.add(new Image(skin.getPatch("table-separator-line"))).width(2).pad(10).padTop(20).padBottom(20).padLeft(0).padRight(0);
-    innerTable.add(new LinkLabel(languagePathRight, FontType.BODY_1.getWhiteVariantName(), actionRight)).pad(10);
+    leftLinkLabel.makeItLink(newActionLeft);
+    rightLinkLabel.makeItLink(newActionRight);
+    innerTable.add(leftLinkLabel).pad(10);
+    NinePatch separator = skin.getPatch("tabs-separator-line");
+    separator.setColor(GRAY.getColor(ColorLevel._500));
+    innerTable.add(new Image(separator)).width(2).pad(10).padTop(20).padBottom(20).padLeft(0).padRight(0);
+    innerTable.add(rightLinkLabel).pad(10);
+    newActionLeft.run();
   }
 }
--- core/src/main/java/pl/agh/edu/ui/frame/TestFrame.java
+++ core/src/main/java/pl/agh/edu/ui/frame/TestFrame.java
@@ -6,12 +6,16 @@ import com.badlogic.gdx.utils.Align;
 
 import pl.agh.edu.engine.bank.BankAccount;
 import pl.agh.edu.engine.bank.BankAccountHandler;
+import pl.agh.edu.ui.component.tab.TabSelector;
 import pl.agh.edu.ui.component.table.CreditTable;
 
 public class TestFrame extends BaseFrame {
 	public TestFrame(String languagePath) {
 		super(languagePath);
 
+
+		// CUSTOM AND LANGUAGE LABELS
+		//
 		// CustomLabel label = new CustomLabel("body1");
 		// label.setText("test");
 		// mainTable.add(label);
@@ -28,21 +32,27 @@ public class TestFrame extends BaseFrame {
 		// languageLabel1.makeItLink(() -> System.out.println("test"));
 		// languageLabel1.setDisabled(true);
 
-		BankAccount bankAccount = new BankAccount(BigDecimal.ONE, BigDecimal.TEN, BigDecimal.TEN);
-		BankAccountHandler bankAccountHandler = new BankAccountHandler(bankAccount);
-		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
-		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
-		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
-		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
-		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
-		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
-		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
-		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
-		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
-		CreditTable creditTable = new CreditTable(bankAccountHandler);
 
-		creditTable.align(Align.left);
-		mainTable.add(creditTable).grow().align(Align.topLeft);
+//		CREDIT TABLE
+//
+//		BankAccount bankAccount = new BankAccount(BigDecimal.ONE, BigDecimal.TEN, BigDecimal.TEN);
+//		BankAccountHandler bankAccountHandler = new BankAccountHandler(bankAccount);
+//		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
+//		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
+//		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
+//		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
+//		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
+//		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
+//		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
+//		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
+//		bankAccountHandler.registerCredit(BigDecimal.valueOf(100000), 10);
+//		CreditTable creditTable = new CreditTable(bankAccountHandler);
+//
+//		creditTable.align(Align.left);
+//		mainTable.add(creditTable).grow().align(Align.topLeft);
+
+		mainTable.add(new TabSelector("test.test", "test.test", () -> System.out.println("left"), () -> System.out.println("right"))).row();
+
 
 	}
 }
