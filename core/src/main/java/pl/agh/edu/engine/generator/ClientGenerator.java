19f01aa94460b40f4fd3cf1e06fa0da2f0d4850d
Paweł Gacek <pawelgacek@student.agh.edu.pl>
2023-10-29 12:57:24 +0100

    HS-217 [Review]

--- core/src/main/java/pl/agh/edu/engine/generator/ClientGenerator.java
+++ core/src/main/java/pl/agh/edu/engine/generator/ClientGenerator.java
@@ -27,7 +27,7 @@ public class ClientGenerator {
 
 	private static ClientGenerator clientGeneratorInstance;
 	private final Time time = Time.getInstance();
-	private static final Faker faker = new Faker(new Locale("pl-PL"));
+	private static final Faker faker = new Faker(new Locale("en-GB"));
 	// Set user input here (set hotelType)
 	private final GameDifficultyManager gameDifficultyManager = GameDifficultyManager.getInstance();
 
