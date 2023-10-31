2cfacaa9cd5c977383b53c65c5398b67a3660441
Jakub Spólnik <jakubspolnik@student.agh.edu.pl>
2023-10-31 23:11:30 +0100

    HS-246 | Fix for linter

--- core/src/main/java/pl/agh/edu/ui/component/selection/BaseSelection.java
+++ core/src/main/java/pl/agh/edu/ui/component/selection/BaseSelection.java
@@ -1,97 +1,114 @@
 package pl.agh.edu.ui.component.selection;
 
+import static com.badlogic.gdx.utils.Align.center;
+
+import java.util.function.Consumer;
+
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import static com.badlogic.gdx.utils.Align.center;
+
 import pl.agh.edu.ui.GameSkin;
 import pl.agh.edu.ui.audio.SoundAudio;
 import pl.agh.edu.ui.utils.wrapper.WrapperTable;
 
-import java.util.function.Consumer;
-
 public abstract class BaseSelection<T> extends WrapperTable {
-    protected final Skin skin = GameSkin.getInstance();
-    private final Button leftButton = new Button(skin, "selection-left");
-    private final Button rightButton = new Button(skin, "selection-right");
-    private T value;
-    private final Label label;
-    public BaseSelection(T value, Label label, Consumer<T> action){
-        this.value = value;
-        this.label = label;
-        leftButton.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                if(!isPreviousButtonCheck()){
-                    leftButton.setDisabled(true);
-                    return true;
-                }
-                return !leftButton.isDisabled();
-            }
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                if(leftButton.isDisabled()) return;
-                SoundAudio.BUTTON_3.play();
-                rightButton.setDisabled(false);
-                previousButtonHandler();
-                updateLabel(label);
-                action.accept(getValue());
-            }
-        });
+	protected final Skin skin = GameSkin.getInstance();
+	private final Button leftButton = new Button(skin, "selection-left");
+	private final Button rightButton = new Button(skin, "selection-right");
+	private T value;
+	private final Label label;
 
-        rightButton.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                if(!isNextButtonCheck()){
-                    rightButton.setDisabled(true);
-                    return true;
-                }
-                return !rightButton.isDisabled();
-            }
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                if(rightButton.isDisabled()) return;
-                SoundAudio.BUTTON_3.play();
-                leftButton.setDisabled(false);
-                nextButtonHandler();
-                updateLabel(label);
-                action.accept(getValue());
-            }
-        });
-        label.setAlignment(center,center);
-        updateLabel(label);
+	public BaseSelection(T value, Label label, Consumer<T> action) {
+		this.value = value;
+		this.label = label;
+		leftButton.addListener(new InputListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				if (!isPreviousButtonCheck()) {
+					leftButton.setDisabled(true);
+					return true;
+				}
+				return !leftButton.isDisabled();
+			}
 
-        innerTable.setFillParent(false);
+			@Override
+			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+				if (leftButton.isDisabled())
+					return;
+				SoundAudio.BUTTON_3.play();
+				rightButton.setDisabled(false);
+				previousButtonHandler();
+				updateLabel(label);
+				action.accept(getValue());
+			}
+		});
 
-        innerTable.add().uniform();
-        innerTable.add(leftButton).uniform();
-        innerTable.add(label).grow();
-        innerTable.add(rightButton).uniform();
-        innerTable.add().uniform().row();
-    }
-    public void changeStyleToBig(){
-        leftButton.setStyle(skin.get("selection-left-big", Button.ButtonStyle.class));
-        rightButton.setStyle(skin.get("selection-right-big", Button.ButtonStyle.class));
-    }
-    protected abstract boolean isNextButtonCheck();
-    protected abstract boolean isPreviousButtonCheck();
-    protected abstract void nextButtonHandler();
-    protected abstract void previousButtonHandler();
-    protected abstract void updateLabel(Label label);
-    public void updateLabel(){
-        updateLabel(label);
-    }
-    public T getValue() {
-        return value;
-    }
-    protected void setValue(T value) {
-        this.value = value;
-    }
-    protected void checkButtons(){
-        rightButton.setDisabled(!isNextButtonCheck());
-        leftButton.setDisabled(!isPreviousButtonCheck());
-    }
+		rightButton.addListener(new InputListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				if (!isNextButtonCheck()) {
+					rightButton.setDisabled(true);
+					return true;
+				}
+				return !rightButton.isDisabled();
+			}
+
+			@Override
+			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+				if (rightButton.isDisabled())
+					return;
+				SoundAudio.BUTTON_3.play();
+				leftButton.setDisabled(false);
+				nextButtonHandler();
+				updateLabel(label);
+				action.accept(getValue());
+			}
+		});
+		label.setAlignment(center, center);
+		updateLabel(label);
+
+		innerTable.setFillParent(false);
+
+		innerTable.add().uniform();
+		innerTable.add(leftButton).uniform();
+		innerTable.add(label).grow();
+		innerTable.add(rightButton).uniform();
+		innerTable.add().uniform().row();
+	}
+
+	public void changeStyleToBig() {
+		leftButton.setStyle(skin.get("selection-left-big", Button.ButtonStyle.class));
+		rightButton.setStyle(skin.get("selection-right-big", Button.ButtonStyle.class));
+	}
+
+	protected abstract boolean isNextButtonCheck();
+
+	protected abstract boolean isPreviousButtonCheck();
+
+	protected abstract void nextButtonHandler();
+
+	protected abstract void previousButtonHandler();
+
+	protected abstract void updateLabel(Label label);
+
+	public void updateLabel() {
+		updateLabel(label);
+	}
+
+	public T getValue() {
+		return value;
+	}
+
+	protected void setValue(T value) {
+		this.value = value;
+	}
+
+	protected void checkButtons() {
+		rightButton.setDisabled(!isNextButtonCheck());
+		leftButton.setDisabled(!isPreviousButtonCheck());
+	}
 
 }
--- core/src/main/java/pl/agh/edu/ui/component/selection/YearSelection.java
+++ core/src/main/java/pl/agh/edu/ui/component/selection/YearSelection.java
@@ -1,54 +1,62 @@
 package pl.agh.edu.ui.component.selection;
 
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import pl.agh.edu.engine.time.Time;
-import pl.agh.edu.ui.component.label.CustomLabel;
 import static pl.agh.edu.ui.utils.FontType.BODY_1;
 
 import java.time.YearMonth;
 import java.util.function.Consumer;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+
+import pl.agh.edu.engine.time.Time;
+import pl.agh.edu.ui.component.label.CustomLabel;
+
 public class YearSelection extends BaseSelection<YearMonth> {
 
-    private final Time time = Time.getInstance();
-    private final boolean isBlockedByTime;
+	private final Time time = Time.getInstance();
+	private final boolean isBlockedByTime;
 
-    public YearSelection(YearMonth startValue, Consumer<YearMonth> action,Boolean isBlockedByTime) {
-        super(startValue, createNewLabel(), action);
-        this.isBlockedByTime = isBlockedByTime;
-    }
+	public YearSelection(YearMonth startValue, Consumer<YearMonth> action, Boolean isBlockedByTime) {
+		super(startValue, createNewLabel(), action);
+		this.isBlockedByTime = isBlockedByTime;
+	}
 
-    private static CustomLabel createNewLabel(){
-        return new CustomLabel(BODY_1.getName());
-    }
+	private static CustomLabel createNewLabel() {
+		return new CustomLabel(BODY_1.getName());
+	}
 
-    public void updateState(YearMonth yearMonth){
-        setValue(yearMonth);
-        this.updateLabel();
-        this.checkButtons();
-    }
-    @Override
-    protected boolean isNextButtonCheck() {
-        if(isBlockedByTime)return getValue().plusYears(1).isBefore(time.getYearMonth().plusYears(2));
-        return true;
-    }
-    @Override
-    protected boolean isPreviousButtonCheck() {
-        if(isBlockedByTime)return (getValue().minusYears(1).isAfter(time.getYearMonth().minusYears(2))
-                && !getValue().minusYears(1).isBefore(YearMonth.from(time.startingTime)));
-        return true;
-    }
-    @Override
-    protected void nextButtonHandler() {
-        setValue(getValue().plusYears(1));
-    }
+	public void updateState(YearMonth yearMonth) {
+		setValue(yearMonth);
+		this.updateLabel();
+		this.checkButtons();
+	}
 
-    @Override
-    protected void previousButtonHandler() {
-        setValue(getValue().minusYears(1));
-    }
-    @Override
-    protected void updateLabel(Label label) {
-        label.setText(String.valueOf(getValue().getYear()));
-    }
+	@Override
+	protected boolean isNextButtonCheck() {
+		if (isBlockedByTime)
+			return getValue().plusYears(1).isBefore(time.getYearMonth().plusYears(2));
+		return true;
+	}
+
+	@Override
+	protected boolean isPreviousButtonCheck() {
+		if (isBlockedByTime)
+			return (getValue().minusYears(1).isAfter(time.getYearMonth().minusYears(2))
+					&& !getValue().minusYears(1).isBefore(YearMonth.from(time.startingTime)));
+		return true;
+	}
+
+	@Override
+	protected void nextButtonHandler() {
+		setValue(getValue().plusYears(1));
+	}
+
+	@Override
+	protected void previousButtonHandler() {
+		setValue(getValue().minusYears(1));
+	}
+
+	@Override
+	protected void updateLabel(Label label) {
+		label.setText(String.valueOf(getValue().getYear()));
+	}
 }
