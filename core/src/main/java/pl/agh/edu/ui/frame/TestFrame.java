3ff1df8bc35d8dd66881bdb6abf555e0f70e94a1
Bartosz Dudek <dudekb@student.agh.edu.pl>
2023-11-02 15:39:22 +0100

    spotless

--- core/src/main/java/pl/agh/edu/ui/frame/TestFrame.java
+++ core/src/main/java/pl/agh/edu/ui/frame/TestFrame.java
@@ -2,21 +2,20 @@ package pl.agh.edu.ui.frame;
 
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
+
 import pl.agh.edu.engine.hotel.HotelType;
 import pl.agh.edu.ui.component.button.ScenarioButton;
-import pl.agh.edu.ui.component.tab.TabSelector;
 
 public class TestFrame extends BaseFrame {
 	public TestFrame(String languagePath) {
 		super(languagePath);
-//		mainTable.add(new TabSelector("test.test", "test.test", () -> System.out.println("left"), () -> System.out.println("right"))).row();
+		// mainTable.add(new TabSelector("test.test", "test.test", () -> System.out.println("left"), () -> System.out.println("right"))).row();
 
 		ButtonGroup<Button> buttons = new ButtonGroup();
 
 		ScenarioButton btn1 = new ScenarioButton(HotelType.RESORT);
 		ScenarioButton btn2 = new ScenarioButton(HotelType.HOTEL);
 		ScenarioButton btn3 = new ScenarioButton(HotelType.SANATORIUM);
-
 
 		buttons.add(btn1.getActor(), btn2.getActor(), btn3.getActor());
 		buttons.setMaxCheckCount(1);
