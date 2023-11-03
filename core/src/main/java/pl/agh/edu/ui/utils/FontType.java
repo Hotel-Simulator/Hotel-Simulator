f7c08556d319f286a12a9a32b2ecb35bb817b757
jagodek <godek.main@gmail.com>
2023-11-03 11:15:42 +0100

    HS-248 | Font Update

--- core/src/main/java/pl/agh/edu/ui/utils/FontType.java
+++ core/src/main/java/pl/agh/edu/ui/utils/FontType.java
@@ -3,6 +3,7 @@ package pl.agh.edu.ui.utils;
 public enum FontType {
 	BODY_1("body1"),
 	BODY_2("body2"),
+	BODY_3("body3"),
 	BUTTON_1("button1"),
 	BUTTON_2("button2"),
 	BUTTON_3("button3"),
@@ -12,7 +13,8 @@ public enum FontType {
 	H3("h3"),
 	H4("h4"),
 	SUBTITLE1("subtitle1"),
-	SUBTITLE2("subtitle2");
+	SUBTITLE2("subtitle2"),
+	SUBTITLE3("subtitle3");
 
 	private final String name;
 
