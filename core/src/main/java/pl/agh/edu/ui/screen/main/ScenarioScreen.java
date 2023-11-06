e1c981ee5f84193c7d2f5f88bb3b32c58adf1c70
Bartosz Dudek <dudekb@student.agh.edu.pl>
2023-11-06 22:16:16 +0100

    tmp

---
+++ core/src/main/java/pl/agh/edu/ui/screen/main/ScenarioScreen.java
@@ -0,0 +1,71 @@
+package pl.agh.edu.ui.screen.main;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+import pl.agh.edu.GdxGame;
+import pl.agh.edu.config.GraphicConfig;
+import pl.agh.edu.ui.GameSkin;
+import pl.agh.edu.ui.panel.ScenarioPanel;
+import pl.agh.edu.ui.resolution.ResolutionChangeListener;
+import pl.agh.edu.ui.resolution.ResolutionManager;
+import pl.agh.edu.utils.MyInputAdapter;
+
+public class ScenarioScreen implements Screen {
+	private Stage stage = new Stage(GraphicConfig.getViewport());
+	public final Skin skin = GameSkin.getInstance();
+	private final GdxGame game;
+
+	public ScenarioScreen(GdxGame game) {
+		this.game = game;
+		stage.getViewport().update(GraphicConfig.getResolution().WIDTH, GraphicConfig.getResolution().HEIGHT, true);
+		ScenarioPanel frame = new ScenarioPanel();
+		stage.addActor(frame.getActor());
+		frame.debug();
+
+		InputProcessor inputMultiplexer = new MyInputAdapter(stage);
+		Gdx.input.setInputProcessor(inputMultiplexer);
+	}
+
+	@Override
+	public void show() {
+		// InputMultiplexer multiplexer = new InputMultiplexer();
+		// multiplexer.addProcessor(stage);
+		// Gdx.input.setInputProcessor(multiplexer);
+	}
+
+	@Override
+	public void render(float delta) {
+		stage.act();
+		stage.draw();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		stage.getViewport().update(width, height, true);
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void hide() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+}
